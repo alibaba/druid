701c0cdebdd19c76aa980b92e3b40c3c26f80172
wenshao <szujobs@hotmail.com>
2011-05-11 06:41:39 +0000

    修改License
    git-svn-id: http://code.alibabatech.com/svn/druid/trunk@8 b9813039-fb51-4c41-a8b9-e21c2acb5095

--- src/main/java/com/alibaba/druid/stat/JdbcStatManagerFactoryBean.java
+++ src/main/java/com/alibaba/druid/stat/JdbcStatManagerFactoryBean.java
@@ -1,17 +1,17 @@
-/**
- * Project: druid
- * 
- * File Created at 2010-12-2
- * $Id$
- * 
- * Copyright 1999-2100 Alibaba.com Corporation Limited.
- * All rights reserved.
+/*
+ * Copyright 2011 Alibaba Group.
  *
- * This software is the confidential and proprietary information of
- * Alibaba Company. ("Confidential Information").  You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Alibaba.com.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.alibaba.druid.stat;
 
