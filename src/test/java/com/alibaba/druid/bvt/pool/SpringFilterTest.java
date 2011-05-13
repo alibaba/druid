5852c172708a1240d02b5e984761596f22848fce
wenshao <szujobs@hotmail.com>
2011-05-13 10:54:00 +0000

    添加测试
    git-svn-id: http://code.alibabatech.com/svn/druid/trunk@54 b9813039-fb51-4c41-a8b9-e21c2acb5095

---
+++ src/test/java/com/alibaba/druid/bvt/pool/SpringFilterTest.java
@@ -0,0 +1,45 @@
+package com.alibaba.druid.bvt.pool;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.sql.DataSource;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.alibaba.druid.filter.FilterAdapter;
+import com.alibaba.druid.filter.FilterChain;
+import com.alibaba.druid.proxy.jdbc.ConnectionProxy;
+
+public class SpringFilterTest extends TestCase {
+    public void test_spring() throws Exception {
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/alibaba/druid/pool/spring-config-1.xml");
+        
+        DataSource dataSource = (DataSource) context.getBean("dataSource");
+        Connection conn = dataSource.getConnection();
+        conn.close();
+        
+        TestFilter filter = (TestFilter) context.getBean("test-filter");
+        Assert.assertEquals(1, filter.getConnectCount());
+    }
+    
+    public static class TestFilter extends FilterAdapter {
+        private AtomicLong connectCount = new AtomicLong();
+        
+        @Override
+        public ConnectionProxy connection_connect(FilterChain chain, Properties info) throws SQLException {
+            connectCount.incrementAndGet();
+            return chain.connection_connect(info);
+        }
+        
+        public long getConnectCount() {
+            return connectCount.get();
+        }
+    }
+}
