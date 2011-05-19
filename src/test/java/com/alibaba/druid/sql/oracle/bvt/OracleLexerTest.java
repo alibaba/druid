5cf13679aaea2197ab6e63d0464f599626c7b694
wenshao <szujobs@hotmail.com>
2011-05-19 13:18:53 +0000

    git-svn-id: http://code.alibabatech.com/svn/druid/trunk@143 b9813039-fb51-4c41-a8b9-e21c2acb5095

--- src/test/java/com/alibaba/druid/sql/oracle/bvt/OracleLexerTest.java
+++ src/test/java/com/alibaba/druid/sql/oracle/bvt/OracleLexerTest.java
@@ -8,27 +8,27 @@ import com.alibaba.druid.sql.parser.Token;
 public class OracleLexerTest extends TestCase {
 
     public void test_hint() throws Exception {
-String sql = "SELECT /*+FIRST_ROWS*/ * FROM T WHERE F1 = ? ORDER BY F2";
-OracleLexer lexer = new OracleLexer(sql);
-for (;;) {
-    lexer.nextToken();
-    Token tok = lexer.token();
+        String sql = "SELECT /*+FIRST_ROWS*/ * FROM T WHERE F1 = ? ORDER BY F2";
+        OracleLexer lexer = new OracleLexer(sql);
+        for (;;) {
+            lexer.nextToken();
+            Token tok = lexer.token();
 
-    switch (tok) {
-        case IDENTIFIER:
-            System.out.println(tok.name() + "\t\t" + lexer.stringVal());
-            break;
-        case HINT:
-            System.out.println(tok.name() + "\t\t\t" + lexer.stringVal());
-            break;
-        default:
-            System.out.println(tok.name() + "\t\t\t" + tok.name);
-            break;
-    }
+            switch (tok) {
+                case IDENTIFIER:
+                    System.out.println(tok.name() + "\t\t" + lexer.stringVal());
+                    break;
+                case HINT:
+                    System.out.println(tok.name() + "\t\t\t" + lexer.stringVal());
+                    break;
+                default:
+                    System.out.println(tok.name() + "\t\t\t" + tok.name);
+                    break;
+            }
 
-    if (tok == Token.EOF) {
-        break;
-    }
-}
+            if (tok == Token.EOF) {
+                break;
+            }
+        }
     }
 }
