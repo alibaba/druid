03e79dd8527cda5e0744617f24ad4520f3694283
wenshao <szujobs@hotmail.com>
2011-05-19 11:45:20 +0000

    修正BVT
    git-svn-id: http://code.alibabatech.com/svn/druid/trunk@136 b9813039-fb51-4c41-a8b9-e21c2acb5095

--- src/test/java/com/alibaba/druid/pool/Case0.java
+++ src/test/java/com/alibaba/druid/pool/Case0.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.alibaba.druid.bvt.pool;
+package com.alibaba.druid.pool;
 
 import java.sql.Connection;
 
