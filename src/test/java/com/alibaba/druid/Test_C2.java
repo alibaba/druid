d6044b444289c7f6e3833982d2c5b06e719b0c3a
wenshao <szujobs@hotmail.com>
2011-05-19 11:42:46 +0000

    git-svn-id: http://code.alibabatech.com/svn/druid/trunk@135 b9813039-fb51-4c41-a8b9-e21c2acb5095

--- src/test/java/com/alibaba/druid/Test_C2.java
+++ src/test/java/com/alibaba/druid/Test_C2.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.alibaba.druid.bvt.pool;
+package com.alibaba.druid;
 
 import java.sql.Connection;
 import java.sql.SQLException;
