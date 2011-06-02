5661b1d2fb4ab54a46c98d3c6c1c11fafaeefcda
wenshao <szujobs@hotmail.com>
2011-06-02 10:31:54 +0000

    git-svn-id: http://code.alibabatech.com/svn/druid/trunk@247 b9813039-fb51-4c41-a8b9-e21c2acb5095

---
+++ src/main/java/com/alibaba/druid/ha/HADataSource.java
@@ -0,0 +1,54 @@
+package com.alibaba.druid.ha;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+public class HADataSource implements DataSource {
+
+    private int         loginTimeout = 0;
+    private PrintWriter logWriter    = new PrintWriter(System.out);
+
+    @Override
+    public PrintWriter getLogWriter() throws SQLException {
+        return logWriter;
+    }
+
+    @Override
+    public void setLogWriter(PrintWriter out) throws SQLException {
+        this.logWriter = out;
+    }
+
+    @Override
+    public void setLoginTimeout(int seconds) throws SQLException {
+        loginTimeout = seconds;
+    }
+
+    @Override
+    public int getLoginTimeout() throws SQLException {
+        return loginTimeout;
+    }
+
+    @Override
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public Connection getConnection() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Connection getConnection(String username, String password) throws SQLException {
+        return null;
+    }
+
+}
