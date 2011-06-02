7641fca35250b11d6e35924892aeb4e7a0e4a843
wenshao <szujobs@hotmail.com>
2011-06-02 02:11:18 +0000

    jconsole
    git-svn-id: http://code.alibabatech.com/svn/druid/trunk@236 b9813039-fb51-4c41-a8b9-e21c2acb5095

--- src/main/java/com/alibaba/druid/jconsole/SQLPanel.java
+++ src/main/java/com/alibaba/druid/jconsole/SQLPanel.java
@@ -1,12 +1,85 @@
 package com.alibaba.druid.jconsole;
 
+import java.awt.BorderLayout;
+
+import javax.management.MBeanServerConnection;
+import javax.management.ObjectInstance;
+import javax.management.openmbean.CompositeData;
+import javax.management.openmbean.TabularData;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class SQLPanel extends JPanel {
 
-    private static final long serialVersionUID = 1L;
+    private static final long     serialVersionUID = 1L;
 
-    public SQLPanel(){
+    private MBeanServerConnection connection;
+    private ObjectInstance        objectInstance;
+    private DataSourceInfo        dataSourceInfo;
 
+    private JTable                table;
+
+    private String[]              columns          = { "ID", "DataSource", "SQL", "ExecuteCount", "ErrorCount", "TotalTime", "LastTime", "MaxTimespan",
+            "LastError", "EffectedRowCount", "FetchRowCount", "MaxTimespanOccurTime", "BatchSizeMax", "BatchSizeTotal", "ConcurrentMax", "RunningCount",
+            "Name", "File", "LastErrorMessage", "LastErrorClass", "LastErrorStackTrace", "LastErrorTime", "DbType" };
+
+    public SQLPanel(MBeanServerConnection connection, ObjectInstance objectInstance, DataSourceInfo dataSourceInfo){
+        super();
+        this.connection = connection;
+        this.objectInstance = objectInstance;
+        this.dataSourceInfo = dataSourceInfo;
+
+        try {
+            TabularData connectionTabularData = (TabularData) connection.getAttribute(objectInstance.getObjectName(), "ConnectionList");
+
+            Object[][] rows = new Object[connectionTabularData.size()][];
+            int rowIndex = 0;
+            for (Object item : connectionTabularData.values()) {
+                CompositeData rowData = (CompositeData) item;
+                Object[] row = new Object[columns.length];
+                row[0] = rowData.get("id");
+                row[1] = rowData.get("connectTime");
+                row[2] = rowData.get("connectTimespan");
+                row[3] = rowData.get("establishTime");
+                row[4] = rowData.get("aliveTimespan");
+
+                row[5] = rowData.get("lastSql");
+                row[6] = rowData.get("lastError");
+                row[7] = rowData.get("lastErrorTime");
+                row[8] = rowData.get("connectStatckTrace");
+                row[9] = rowData.get("lastStatementStackTrace");
+
+                row[10] = rowData.get("dataSource");
+
+                rows[rowIndex++] = row;
+            }
+
+            table = new JTable(rows, columns);
+
+            JScrollPane tableScrollPane = new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+            this.setLayout(new BorderLayout());
+            this.add(tableScrollPane, BorderLayout.CENTER);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    
+    public MBeanServerConnection getConnection() {
+        return connection;
+    }
+
+    
+    public ObjectInstance getObjectInstance() {
+        return objectInstance;
+    }
+
+    
+    public DataSourceInfo getDataSourceInfo() {
+        return dataSourceInfo;
+    }
+
+    
 }
