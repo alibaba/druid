de315db871edc5c1be4256849da2f0b68c706534
wenshao <szujobs@hotmail.com>
2011-06-03 07:09:55 +0000

    git-svn-id: http://code.alibabatech.com/svn/druid/trunk@257 b9813039-fb51-4c41-a8b9-e21c2acb5095

--- src/main/java/com/alibaba/druid/VERSION.java
+++ src/main/java/com/alibaba/druid/VERSION.java
@@ -19,5 +19,5 @@ public interface VERSION {
 
     public final static int MajorVersion    = 0;
     public final static int MinorVersion    = 1;
-    public final static int RevisionVersion = 1;
+    public final static int RevisionVersion = 2;
 }
