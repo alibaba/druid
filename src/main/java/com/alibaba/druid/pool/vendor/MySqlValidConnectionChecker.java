4f97ded8d47da830c9d2c11e232369a45eb0c6c5
wenshao <szujobs@hotmail.com>
2011-06-29 05:42:15 +0000

    重构
    git-svn-id: http://code.alibabatech.com/svn/druid/trunk@296 b9813039-fb51-4c41-a8b9-e21c2acb5095

--- src/main/java/com/alibaba/druid/pool/vendor/MySqlValidConnectionChecker.java
+++ src/main/java/com/alibaba/druid/pool/vendor/MySqlValidConnectionChecker.java
@@ -12,10 +12,10 @@ import com.alibaba.druid.logging.LogFactory;
 import com.alibaba.druid.pool.ValidConnectionChecker;
 import com.alibaba.druid.util.JdbcUtils;
 
-public class MySqlValidConnectionChecker1 implements ValidConnectionChecker, Serializable {
+public class MySqlValidConnectionChecker implements ValidConnectionChecker, Serializable {
 
     private static final long serialVersionUID    = 1L;
-    private static final Log  LOG                 = LogFactory.getLog(MySqlValidConnectionChecker1.class);
+    private static final Log  LOG                 = LogFactory.getLog(MySqlValidConnectionChecker.class);
 
     private Method            ping;
     private boolean           driverHasPingMethod = false;
@@ -24,7 +24,7 @@ public class MySqlValidConnectionChecker1 implements ValidConnectionChecker, Ser
     private static Object[]   params              = new Object[] {};
 
     @SuppressWarnings({ "unchecked", "rawtypes" })
-    public MySqlValidConnectionChecker1(){
+    public MySqlValidConnectionChecker(){
         try {
             Class mysqlConnection = Thread.currentThread().getContextClassLoader().loadClass("com.mysql.jdbc.Connection");
             ping = mysqlConnection.getMethod("ping", new Class[] {});
