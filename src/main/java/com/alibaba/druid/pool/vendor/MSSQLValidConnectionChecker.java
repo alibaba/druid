ceb1ae96f702692ab587d59f5324f4f92347a28b
wenshao <szujobs@hotmail.com>
2011-06-29 07:22:38 +0000

    git-svn-id: http://code.alibabatech.com/svn/druid/trunk@302 b9813039-fb51-4c41-a8b9-e21c2acb5095

--- src/main/java/com/alibaba/druid/pool/vendor/MSSQLValidConnectionChecker.java
+++ src/main/java/com/alibaba/druid/pool/vendor/MSSQLValidConnectionChecker.java
@@ -1,24 +1,3 @@
-/*
- * JBoss, Home of Professional Open Source.
- * Copyright 2006, Red Hat Middleware LLC, and individual contributors
- * as indicated by the @author tags. See the copyright.txt file in the
- * distribution for a full listing of individual contributors.
- *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- */
 package com.alibaba.druid.pool.vendor;
 
 import java.io.Serializable;
@@ -33,9 +12,6 @@ import com.alibaba.druid.util.JdbcUtils;
 
 /**
  * A MSSQLValidConnectionChecker.
- * 
- * @author <a href="weston.price@jboss.com">Weston Price</a>
- * @version $Revision: 57189 $
  */
 public class MSSQLValidConnectionChecker implements ValidConnectionChecker, Serializable {
 
