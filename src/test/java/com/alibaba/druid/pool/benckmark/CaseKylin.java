cde7e3fa7c3ed53bb9745b5d3f3a41047cab70c4
wenshao <szujobs@hotmail.com>
2011-07-12 08:41:13 +0000

    kylin支持
    git-svn-id: http://code.alibabatech.com/svn/druid/trunk@378 b9813039-fb51-4c41-a8b9-e21c2acb5095

---
+++ src/test/java/com/alibaba/druid/pool/benckmark/CaseKylin.java
@@ -0,0 +1,145 @@
+package com.alibaba.druid.pool.benckmark;
+
+import java.sql.Connection;
+import java.text.NumberFormat;
+import java.util.concurrent.CountDownLatch;
+
+import javax.sql.DataSource;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.dbcp.BasicDataSource;
+
+import com.alibaba.druid.TestUtil;
+import com.alibaba.druid.pool.DruidDataSource;
+
+public class CaseKylin extends TestCase {
+
+    private String  jdbcUrl;
+    private String  user;
+    private String  password;
+    private String  driverClass;
+    private int     initialSize                   = 1;
+    private int     minIdle                       = 1;
+    private int     maxIdle                       = 14;
+    private int     maxActive                     = 14;
+    private int     maxWait                       = 60000;
+    private String  validationQuery               = null;      // "SELECT 1";
+    private int     threadCount                   = 40;
+    private int     TEST_COUNT                    = 3;
+    final int       LOOP_COUNT                    = 1000 * 100;
+    private boolean testOnBorrow                  = true;
+
+    private boolean removeAbandoned               = true;
+    private int     removeAbandonedTimeout        = 180;
+    private long    timeBetweenEvictionRunsMillis = 60000;
+    private long    minEvictableIdleTimeMillis    = 60000;
+
+    protected void setUp() throws Exception {
+        // jdbcUrl = "jdbc:fake:dragoon_v25masterdb";
+        // user = "dragoon25";
+        // password = "dragoon25";
+        // driverClass = "com.alibaba.druid.mock.MockDriver";
+
+        jdbcUrl = "jdbc:mysql://10.20.153.104:3306/druid2";
+        user = "root";
+        password = "root";
+    }
+
+    public void test_perf() throws Exception {
+        for (int i = 0; i < 10; ++i) {
+            druid();
+            dbcp();
+        }
+    }
+
+    public void druid() throws Exception {
+        DruidDataSource dataSource = new DruidDataSource();
+
+        dataSource.setInitialSize(initialSize);
+        dataSource.setMaxActive(maxActive);
+        dataSource.setMaxIdle(maxIdle);
+        dataSource.setMinIdle(minIdle);
+        dataSource.setMaxWait(maxWait);
+        dataSource.setPoolPreparedStatements(true);
+        dataSource.setDriverClassName(driverClass);
+        dataSource.setUrl(jdbcUrl);
+        dataSource.setPoolPreparedStatements(true);
+        dataSource.setUsername(user);
+        dataSource.setPassword(password);
+        dataSource.setValidationQuery(validationQuery);
+        dataSource.setTestOnBorrow(testOnBorrow);
+        dataSource.setRemoveAbandoned(removeAbandoned);
+        dataSource.setRemoveAbandonedTimeout(removeAbandonedTimeout);
+        dataSource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+        dataSource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+
+        for (int i = 0; i < TEST_COUNT; ++i) {
+            p0(dataSource, "druid", threadCount);
+        }
+        System.out.println();
+    }
+
+    public void dbcp() throws Exception {
+        final BasicDataSource dataSource = new BasicDataSource();
+
+        dataSource.setInitialSize(initialSize);
+        dataSource.setMaxActive(maxActive);
+        dataSource.setMaxIdle(maxIdle);
+        dataSource.setMinIdle(minIdle);
+        dataSource.setMaxWait(maxWait);
+        dataSource.setPoolPreparedStatements(true);
+        dataSource.setDriverClassName(driverClass);
+        dataSource.setUrl(jdbcUrl);
+        dataSource.setPoolPreparedStatements(true);
+        dataSource.setUsername(user);
+        dataSource.setPassword(password);
+        dataSource.setValidationQuery(validationQuery);
+        dataSource.setTestOnBorrow(testOnBorrow);
+        dataSource.setRemoveAbandoned(removeAbandoned);
+        dataSource.setRemoveAbandonedTimeout(removeAbandonedTimeout);
+        dataSource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+        dataSource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+
+        for (int i = 0; i < TEST_COUNT; ++i) {
+            p0(dataSource, "dbcp", threadCount);
+        }
+        System.out.println();
+    }
+
+    private void p0(final DataSource dataSource, String name, int threadCount) throws Exception {
+
+        final CountDownLatch startLatch = new CountDownLatch(1);
+        final CountDownLatch endLatch = new CountDownLatch(1);
+        for (int i = 0; i < threadCount; ++i) {
+            Thread thread = new Thread() {
+
+                public void run() {
+                    try {
+                        startLatch.await();
+
+                        for (int i = 0; i < LOOP_COUNT; ++i) {
+                            Connection conn = dataSource.getConnection();
+                            conn.close();
+                        }
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                    endLatch.countDown();
+                }
+            };
+            thread.start();
+        }
+        long startMillis = System.currentTimeMillis();
+        long startYGC = TestUtil.getYoungGC();
+        long startFullGC = TestUtil.getFullGC();
+        startLatch.countDown();
+        endLatch.await();
+
+        long millis = System.currentTimeMillis() - startMillis;
+        long ygc = TestUtil.getYoungGC() - startYGC;
+        long fullGC = TestUtil.getFullGC() - startFullGC;
+
+        System.out.println("thread " + threadCount + " " + name + " millis : " + NumberFormat.getInstance().format(millis) + ", YGC " + ygc + " FGC " + fullGC);
+    }
+}
