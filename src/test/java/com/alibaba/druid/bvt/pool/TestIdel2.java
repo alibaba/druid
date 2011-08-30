b421605c70727586c5b797a13a4a567cde1615d8
wenshao <szujobs@hotmail.com>
2011-08-30 08:54:11 +0000

    test
    git-svn-id: http://code.alibabatech.com/svn/druid/trunk@544 b9813039-fb51-4c41-a8b9-e21c2acb5095

---
+++ src/test/java/com/alibaba/druid/bvt/pool/TestIdel2.java
@@ -0,0 +1,77 @@
+package com.alibaba.druid.bvt.pool;
+
+import java.sql.Connection;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import com.alibaba.druid.mock.MockDriver;
+import com.alibaba.druid.pool.DruidDataSource;
+
+
+public class TestIdel2 extends TestCase {
+    public void test_idle2() throws Exception {
+        MockDriver driver = new MockDriver();
+
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl("jdbc:mock:xxx");
+        dataSource.setDriver(driver);
+        dataSource.setInitialSize(1);
+        dataSource.setMaxActive(14);
+        dataSource.setMaxIdle(14);
+        dataSource.setMinIdle(1);
+        dataSource.setMinEvictableIdleTimeMillis(50 * 1);
+        dataSource.setTimeBetweenEvictionRunsMillis(10);
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTestOnBorrow(false);
+        dataSource.setValidationQuery("SELECT 1");
+        
+        {
+            Assert.assertEquals(0, dataSource.getCreateCount());
+            Assert.assertEquals(0, dataSource.getActiveCount());
+            
+            Connection conn = dataSource.getConnection();
+
+            Assert.assertEquals(dataSource.getInitialSize(), dataSource.getCreateCount());
+            Assert.assertEquals(dataSource.getInitialSize(), driver.getConnections().size());
+            Assert.assertEquals(1, dataSource.getActiveCount());
+
+            conn.close();
+            Assert.assertEquals(0, dataSource.getDestroyCount());
+            Assert.assertEquals(2, driver.getConnections().size());
+            Assert.assertEquals(2, dataSource.getCreateCount());
+            Assert.assertEquals(0, dataSource.getActiveCount());
+        }
+        
+        {
+            int count = 14;
+            Connection[] connections = new Connection[count];
+            for (int i = 0; i < count; ++i) {
+                connections[i] = dataSource.getConnection();
+                Assert.assertEquals(i + 1, dataSource.getActiveCount());
+            }
+            Assert.assertEquals(dataSource.getMaxActive(), dataSource.getCreateCount());
+            Assert.assertEquals(count, driver.getConnections().size());
+            for (int i = 0; i < count; ++i) {
+                connections[i].close();
+                Assert.assertEquals(count - i - 1, dataSource.getActiveCount());
+            }
+            Assert.assertEquals(dataSource.getMaxActive(), dataSource.getCreateCount());
+            Assert.assertEquals(0, dataSource.getActiveCount());
+            Assert.assertEquals(14, driver.getConnections().size());
+        }
+        
+        for (int i = 0; i < 100; ++i) {
+            Assert.assertEquals(0, dataSource.getActiveCount());
+            Connection conn = dataSource.getConnection();
+            
+            Assert.assertEquals(1, dataSource.getActiveCount());
+            
+            Thread.sleep(1);
+            conn.close();
+        }
+        Assert.assertEquals(2, dataSource.getPoolingCount());
+        
+        dataSource.close();
+    }
+}
