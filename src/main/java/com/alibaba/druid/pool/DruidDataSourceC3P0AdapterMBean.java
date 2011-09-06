21736b2a1f01da3d8892659867579ee82cbf0525
wenshao <szujobs@hotmail.com>
2011-09-06 14:29:42 +0000

    rename
    git-svn-id: http://code.alibabatech.com/svn/druid/trunk@662 b9813039-fb51-4c41-a8b9-e21c2acb5095

---
+++ src/main/java/com/alibaba/druid/pool/DruidDataSourceC3P0AdapterMBean.java
@@ -0,0 +1,48 @@
+package com.alibaba.druid.pool;
+
+public interface DruidDataSourceC3P0AdapterMBean {
+
+    String getUser();
+
+    int getCheckoutTimeout();
+
+    boolean isAutoCommitOnClose();
+
+    int getIdleConnectionTestPeriod();
+
+    int getInitialPoolSize();
+
+    int getMaxIdleTime();
+
+    int getMaxPoolSize();
+
+    int getMinPoolSize();
+
+    boolean isTestConnectionOnCheckout();
+
+    boolean isTestConnectionOnCheckin();
+
+    String getPreferredTestQuery();
+    
+    int getNumIdleConnections();
+    
+    int getNumConnections();
+    
+    String getDataSourceName();
+    
+    int getNumBusyConnections();
+
+    // //////////
+
+    boolean isEnable();
+
+    void shrink();
+
+    String toString();
+
+    int getWaitThreadCount();
+
+    int getLockQueueLength();
+    
+    void close();
+}
