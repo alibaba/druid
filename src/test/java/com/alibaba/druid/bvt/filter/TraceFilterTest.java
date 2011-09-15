b5372a8efa11b88a966bfb9a05900a9b4b8307fb
wenshao <szujobs@hotmail.com>
2011-09-15 05:20:05 +0000

    bvt fixed
    git-svn-id: http://code.alibabatech.com/svn/druid/trunk@748 b9813039-fb51-4c41-a8b9-e21c2acb5095

---
+++ src/test/java/com/alibaba/druid/bvt/filter/TraceFilterTest.java
@@ -0,0 +1,73 @@
+package com.alibaba.druid.bvt.filter;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import com.alibaba.druid.mock.MockDriver;
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.stat.DruidDataSourceStatManager;
+
+public class TraceFilterTest extends TestCase {
+
+    private MockDriver      driver;
+    private DruidDataSource dataSource;
+
+    protected void setUp() throws Exception {
+        Assert.assertEquals(0, DruidDataSourceStatManager.getInstance().getDataSourceList().size());
+
+        driver = new MockDriver();
+
+        dataSource = new DruidDataSource();
+        dataSource.setUrl("jdbc:mock:xxx");
+        dataSource.setDriver(driver);
+        dataSource.setInitialSize(1);
+        dataSource.setMaxActive(2);
+        dataSource.setMaxIdle(2);
+        dataSource.setMinIdle(1);
+        dataSource.setMinEvictableIdleTimeMillis(300 * 1000); // 300 / 10
+        dataSource.setTimeBetweenEvictionRunsMillis(180 * 1000); // 180 / 10
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTestOnBorrow(false);
+        dataSource.setValidationQuery("SELECT 1");
+        dataSource.setFilters("trace");
+    }
+
+    protected void tearDown() throws Exception {
+        dataSource.close();
+        Assert.assertEquals(0, DruidDataSourceStatManager.getInstance().getDataSourceList().size());
+    }
+
+    public void test_exuecute() throws Exception {
+        Connection conn = dataSource.getConnection();
+        Statement stmt = conn.createStatement();
+        stmt.execute("SELECT 1");
+        stmt.close();
+        conn.close();
+    }
+    
+    public void test_exuecuteQuery() throws Exception {
+        Connection conn = dataSource.getConnection();
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT 1");
+        rs.next();
+        rs.close();
+        stmt.close();
+        conn.close();
+    }
+    
+    public void test_preExuecuteQuery() throws Exception {
+        Connection conn = dataSource.getConnection();
+        PreparedStatement stmt = conn.prepareStatement("SELECT ?");
+        stmt.setInt(1, 123);
+        ResultSet rs = stmt.executeQuery();
+        rs.next();
+        rs.close();
+        stmt.close();
+        conn.close();
+    }
+}
