c3f0a0e5de4f580d37cc3fc86c494a19c5577ad3
wenshao <szujobs@hotmail.com>
2011-09-15 07:21:04 +0000

    bvt
    git-svn-id: http://code.alibabatech.com/svn/druid/trunk@755 b9813039-fb51-4c41-a8b9-e21c2acb5095

---
+++ src/test/java/com/alibaba/druid/bvt/pool/basic/PrepareStatementTest.java
@@ -0,0 +1,88 @@
+package com.alibaba.druid.bvt.pool.basic;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import com.alibaba.druid.mock.MockDriver;
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.stat.DruidDataSourceStatManager;
+import com.mysql.jdbc.ResultSet;
+
+public class PrepareStatementTest extends TestCase {
+
+    private MockDriver      driver;
+    private DruidDataSource dataSource;
+
+    protected void setUp() throws Exception {
+        Assert.assertEquals(0, DruidDataSourceStatManager.getInstance().getDataSourceList().size());
+
+        driver = new MockDriver();
+
+        dataSource = new DruidDataSource();
+        dataSource.setUrl("jdbc:mock:xxx");
+        dataSource.setDriver(driver);
+        dataSource.setInitialSize(1);
+        dataSource.setMaxActive(2);
+        dataSource.setMaxIdle(2);
+        dataSource.setMinIdle(1);
+        dataSource.setMinEvictableIdleTimeMillis(300 * 1000); // 300 / 10
+        dataSource.setTimeBetweenEvictionRunsMillis(180 * 1000); // 180 / 10
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTestOnBorrow(false);
+        dataSource.setValidationQuery("SELECT 1");
+        dataSource.setFilters("stat");
+    }
+
+    protected void tearDown() throws Exception {
+        dataSource.close();
+        Assert.assertEquals(0, DruidDataSourceStatManager.getInstance().getDataSourceList().size());
+    }
+
+    public void test_prepare() throws Exception {
+        Connection conn = dataSource.getConnection();
+        
+        PreparedStatement stmt = conn.prepareStatement("SELECT 1", ResultSet.FETCH_FORWARD, ResultSet.CONCUR_READ_ONLY);
+        stmt.close();
+        
+        conn.close();
+    }
+    
+    public void test_prepare2() throws Exception {
+        Connection conn = dataSource.getConnection();
+        
+        PreparedStatement stmt = conn.prepareStatement("SELECT 1", ResultSet.FETCH_FORWARD, ResultSet.CONCUR_READ_ONLY, ResultSet.HOLD_CURSORS_OVER_COMMIT);
+        stmt.close();
+        
+        conn.close();
+    }
+    
+    public void test_prepareCall() throws Exception {
+        Connection conn = dataSource.getConnection();
+        
+        PreparedStatement stmt = conn.prepareCall("SELECT 1");
+        stmt.close();
+        
+        conn.close();
+    }
+    
+    public void test_prepareCall1() throws Exception {
+        Connection conn = dataSource.getConnection();
+        
+        PreparedStatement stmt = conn.prepareCall("SELECT 1", ResultSet.FETCH_FORWARD, ResultSet.CONCUR_READ_ONLY);
+        stmt.close();
+        
+        conn.close();
+    }
+    
+    public void test_prepareCall2() throws Exception {
+        Connection conn = dataSource.getConnection();
+        
+        PreparedStatement stmt = conn.prepareCall("SELECT 1", ResultSet.FETCH_FORWARD, ResultSet.CONCUR_READ_ONLY, ResultSet.HOLD_CURSORS_OVER_COMMIT);
+        stmt.close();
+        
+        conn.close();
+    }
+}
