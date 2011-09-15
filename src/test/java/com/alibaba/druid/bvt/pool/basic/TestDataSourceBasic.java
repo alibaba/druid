2f216310413fbf12aa3fbe474bdd3ba26bdd9918
wenshao <szujobs@hotmail.com>
2011-09-15 07:43:23 +0000

    测试
    git-svn-id: http://code.alibabatech.com/svn/druid/trunk@759 b9813039-fb51-4c41-a8b9-e21c2acb5095

---
+++ src/test/java/com/alibaba/druid/bvt/pool/basic/TestDataSourceBasic.java
@@ -0,0 +1,56 @@
+package com.alibaba.druid.bvt.pool.basic;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import com.alibaba.druid.mock.MockDriver;
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.stat.DruidDataSourceStatManager;
+import com.mysql.jdbc.ResultSet;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class TestDataSourceBasic extends TestCase {
+
+    private MockDriver      driver;
+    private DruidDataSource dataSource;
+
+    protected void setUp() throws Exception {
+        Assert.assertEquals(0, DruidDataSourceStatManager.getInstance().getDataSourceList().size());
+
+        driver = new MockDriver();
+
+        dataSource = new DruidDataSource();
+        dataSource.setUrl("jdbc:mock:xxx");
+        dataSource.setDriver(driver);
+        dataSource.setInitialSize(1);
+        dataSource.setMaxActive(2);
+        dataSource.setMaxIdle(2);
+        dataSource.setMinIdle(1);
+        dataSource.setMinEvictableIdleTimeMillis(300 * 1000); // 300 / 10
+        dataSource.setTimeBetweenEvictionRunsMillis(180 * 1000); // 180 / 10
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTestOnBorrow(false);
+        dataSource.setValidationQuery("SELECT 1");
+        dataSource.setFilters("stat");
+        dataSource.setRemoveAbandoned(true);
+    }
+
+    protected void tearDown() throws Exception {
+        dataSource.close();
+        Assert.assertEquals(0, DruidDataSourceStatManager.getInstance().getDataSourceList().size());
+    }
+
+    public void test_prepare() throws Exception {
+        Connection conn = dataSource.getConnection();
+
+        Assert.assertEquals(1, dataSource.getActiveConnectionStackTrace().size());
+        Assert.assertEquals(1, dataSource.getActiveConnections().size());
+
+        conn.close();
+        
+        Assert.assertEquals(0, dataSource.getActiveConnectionStackTrace().size());
+        Assert.assertEquals(0, dataSource.getActiveConnections().size());
+    }
+}
