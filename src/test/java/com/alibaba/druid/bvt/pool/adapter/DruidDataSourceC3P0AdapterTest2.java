97f669ff5181d12cf7fc28c81eb6729ed8ef5f3a
wenshao <szujobs@hotmail.com>
2011-09-18 14:21:29 +0000

    完善mock和测试
    git-svn-id: http://code.alibabatech.com/svn/druid/trunk@841 b9813039-fb51-4c41-a8b9-e21c2acb5095

---
+++ src/test/java/com/alibaba/druid/bvt/pool/adapter/DruidDataSourceC3P0AdapterTest2.java
@@ -0,0 +1,36 @@
+package com.alibaba.druid.bvt.pool.adapter;
+
+import javax.sql.DataSource;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidDataSourceC3P0Adapter;
+import com.alibaba.druid.stat.DruidDataSourceStatManager;
+
+public class DruidDataSourceC3P0AdapterTest2 extends TestCase {
+
+    public void test_0() throws Exception {
+        Assert.assertEquals(0, DruidDataSourceStatManager.getInstance().getDataSourceList().size());
+
+        DruidDataSourceC3P0Adapter dataSource = new DruidDataSourceC3P0Adapter();
+        dataSource.setJdbcUrl("jdbc:mock:xxx");
+        
+        dataSource.setLogWriter(dataSource.getLogWriter());
+        dataSource.setLoginTimeout(dataSource.getLoginTimeout());
+        
+        Assert.assertTrue(dataSource.isWrapperFor(DruidDataSourceC3P0Adapter.class));
+        Assert.assertTrue(dataSource.isWrapperFor(DruidDataSource.class));
+        Assert.assertTrue(dataSource.isWrapperFor(DataSource.class));
+        Assert.assertFalse(dataSource.isWrapperFor(null));
+
+        dataSource.close();
+    }
+
+    protected void tearDown() throws Exception {
+        for (DruidDataSource dataSource : DruidDataSourceStatManager.getDruidDataSourceInstances()) {
+            dataSource.close();
+        }
+    }
+}
