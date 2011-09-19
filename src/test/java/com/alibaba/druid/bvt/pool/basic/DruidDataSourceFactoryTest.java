e64bcfb80820b48e934099d58ff79eb6c4b0db19
wenshao <szujobs@hotmail.com>
2011-09-19 00:19:16 +0000

    git-svn-id: http://code.alibabatech.com/svn/druid/trunk@844 b9813039-fb51-4c41-a8b9-e21c2acb5095

--- src/test/java/com/alibaba/druid/bvt/pool/basic/DruidDataSourceFactoryTest.java
+++ src/test/java/com/alibaba/druid/bvt/pool/basic/DruidDataSourceFactoryTest.java
@@ -1,6 +1,7 @@
 package com.alibaba.druid.bvt.pool.basic;
 
 import java.util.Hashtable;
+import java.util.Properties;
 
 import javax.naming.RefAddr;
 import javax.naming.Reference;
@@ -39,4 +40,30 @@ public class DruidDataSourceFactoryTest extends TestCase {
         factory.getObjectInstance(ref, null, null, new Hashtable<Object, Object>());
     }
     
+    public void test_createDataSource() throws Exception {
+        Properties properties = new Properties();
+        properties.setProperty("defaultAutoCommit", "true");
+        properties.setProperty("defaultReadOnly", "true");
+        properties.setProperty("defaultTransactionIsolation", "NONE");
+        properties.setProperty("defaultCatalog", "cn");
+        properties.setProperty("driverClassName", "com.alibaba.druid.mock.MockDriver");
+        properties.setProperty("maxActive", "8");
+        properties.setProperty("maxIdle", "8");
+        properties.setProperty("minIdle", "3");
+        properties.setProperty("initialSize", "1");
+        properties.setProperty("maxWait", "-1");
+        properties.setProperty("testOnBorrow", "true");
+        properties.setProperty("testOnReturn", "true");
+        properties.setProperty("timeBetweenEvictionRunsMillis", "3000");
+        properties.setProperty("numTestsPerEvictionRun", "1");
+        properties.setProperty("minEvictableIdleTimeMillis", "10000");
+        properties.setProperty("testWhileIdle", "true");
+        properties.setProperty("password", "xxx");
+        properties.setProperty("url", "jdbc:mock:xxx");
+        properties.setProperty("username", "user");
+        properties.setProperty("validationQuery", "select 1");
+        properties.setProperty("validationQueryTimeout", "30");
+        
+        DruidDataSourceFactory.createDataSource(properties);
+    }
 }
