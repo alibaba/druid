7e713861e773df7a68b3e82609fb008a5c44364d
wenshao <szujobs@hotmail.com>
2011-09-20 10:08:22 +0000

    补充Oracle测试
    git-svn-id: http://code.alibabatech.com/svn/druid/trunk@885 b9813039-fb51-4c41-a8b9-e21c2acb5095

---
+++ src/test/java/com/alibaba/druid/sql/oracle/bvt/visitor/OracleOutputVisitorTest_forupdate.java
@@ -0,0 +1,40 @@
+package com.alibaba.druid.sql.oracle.bvt.visitor;
+
+import java.util.List;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import com.alibaba.druid.sql.ast.SQLStatement;
+import com.alibaba.druid.sql.dialect.oracle.parser.OracleStatementParser;
+import com.alibaba.druid.sql.dialect.oracle.visitor.OracleOutputVisitor;
+import com.alibaba.druid.sql.dialect.oracle.visitor.OracleSchemaStatVisitor;
+import com.alibaba.druid.stat.TableStat.Column;
+
+
+public class OracleOutputVisitorTest_forupdate extends TestCase {
+    public void test_0() throws Exception {
+        String sql = "SELECT salary from employee for update";
+
+        OracleStatementParser parser = new OracleStatementParser(sql);
+        List<SQLStatement> statementList = parser.parseStatementList();
+        SQLStatement stmt = statementList.get(0);
+
+        Assert.assertEquals(1, statementList.size());
+
+        OracleSchemaStatVisitor visitor = new OracleSchemaStatVisitor();
+        stmt.accept(visitor);
+
+        Assert.assertEquals(1, visitor.getTables().size());
+        Assert.assertEquals(true, visitor.containsTable("employee"));
+
+        Assert.assertEquals(1, visitor.getFields().size());
+        Assert.assertEquals(true, visitor.getFields().contains(new Column("employee", "salary")));
+        
+        StringBuilder buf = new StringBuilder();
+        OracleOutputVisitor outputVisitor = new OracleOutputVisitor(buf);
+        stmt.accept(outputVisitor);
+        Assert.assertEquals("SELECT salary\nFROM employee\nFOR UPDATE;\n", buf.toString());
+
+    }
+}
