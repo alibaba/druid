0e411d332afe67944ce939443b55148d5f72e840
wenshao <szujobs@hotmail.com>
2011-10-08 07:56:28 +0000

    git-svn-id: http://code.alibabatech.com/svn/druid/trunk@926 b9813039-fb51-4c41-a8b9-e21c2acb5095

--- src/main/java/com/alibaba/druid/TransactionTimeoutException.java
+++ src/main/java/com/alibaba/druid/TransactionTimeoutException.java
@@ -1,9 +1,9 @@
 package com.alibaba.druid;
 
-import java.sql.SQLTimeoutException;
+import java.sql.SQLException;
 
 
-public class TransactionTimeoutException extends SQLTimeoutException {
+public class TransactionTimeoutException extends SQLException {
 
     private static final long serialVersionUID = 1L;
 
