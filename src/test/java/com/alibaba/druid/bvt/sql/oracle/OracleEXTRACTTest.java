15f7558444e8f2fdf39ed775f42bb0cb1cbafb9e
wenshao <szujobs@hotmail.com>
2011-10-13 15:45:13 +0000

    补充测试
    git-svn-id: http://code.alibabatech.com/svn/druid/trunk@960 b9813039-fb51-4c41-a8b9-e21c2acb5095

---
+++ src/test/java/com/alibaba/druid/bvt/sql/oracle/OracleEXTRACTTest.java
@@ -0,0 +1,28 @@
+package com.alibaba.druid.bvt.sql.oracle;
+
+import com.alibaba.druid.sql.ast.statement.SQLSelectStatement;
+import com.alibaba.druid.sql.dialect.oracle.parser.OracleStatementParser;
+import com.alibaba.druid.sql.test.TestUtils;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class OracleEXTRACTTest extends TestCase {
+
+	public void test_Extract() throws Exception {
+		String sql = "SELECT EXTRACT(YEAR FROM DATE '1998-03-07') FROM DUAL;";
+
+		String expect = "SELECT EXTRACT(YEAR FROM DATE '1998-03-07')\n"
+				+ "FROM DUAL;\n";
+
+		OracleStatementParser parser = new OracleStatementParser(sql);
+		SQLSelectStatement stmt = (SQLSelectStatement) parser
+				.parseStatementList().get(0);
+
+		String text = TestUtils.output(stmt);
+
+		Assert.assertEquals(expect, text);
+
+		System.out.println(text);
+	}
+}
