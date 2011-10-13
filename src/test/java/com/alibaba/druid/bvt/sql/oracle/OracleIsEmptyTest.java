e6c929bb94cc91d12ee302c51e0b40e8904920ed
wenshao <szujobs@hotmail.com>
2011-10-13 10:31:15 +0000

    Oracle Parser支持Model Clause
    git-svn-id: http://code.alibabatech.com/svn/druid/trunk@954 b9813039-fb51-4c41-a8b9-e21c2acb5095

---
+++ src/test/java/com/alibaba/druid/bvt/sql/oracle/OracleIsEmptyTest.java
@@ -0,0 +1,26 @@
+package com.alibaba.druid.bvt.sql.oracle;
+
+import com.alibaba.druid.sql.ast.statement.SQLSelectStatement;
+import com.alibaba.druid.sql.dialect.oracle.parser.OracleStatementParser;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class OracleIsEmptyTest extends TestCase {
+
+    public void test_isEmpty() throws Exception {
+        String sql = "SELECT product_id, TO_CHAR(ad_finaltext) FROM print_media WHERE ad_textdocs_ntab IS NOT EMPTY;";
+
+        String expect = "SELECT product_id, TO_CHAR(ad_finaltext)\n" + "FROM print_media\n"
+                        + "WHERE ad_textdocs_ntab IS NOT EMPTY;\n";
+
+        OracleStatementParser parser = new OracleStatementParser(sql);
+        SQLSelectStatement stmt = (SQLSelectStatement) parser.parseStatementList().get(0);
+
+        String text = TestUtils.output(stmt);
+
+        Assert.assertEquals(expect, text);
+
+        System.out.println(text);
+    }
+}
