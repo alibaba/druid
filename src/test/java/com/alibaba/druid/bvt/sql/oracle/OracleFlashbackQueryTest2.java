eb3a55dc192a88fb1c341c96863f1a3dfe2ebf41
wenshao <szujobs@hotmail.com>
2011-10-14 07:46:55 +0000

    增强测试
    git-svn-id: http://code.alibabatech.com/svn/druid/trunk@962 b9813039-fb51-4c41-a8b9-e21c2acb5095

---
+++ src/test/java/com/alibaba/druid/bvt/sql/oracle/OracleFlashbackQueryTest2.java
@@ -0,0 +1,31 @@
+package com.alibaba.druid.bvt.sql.oracle;
+
+import com.alibaba.druid.sql.ast.statement.SQLSelectStatement;
+import com.alibaba.druid.sql.dialect.oracle.parser.OracleStatementParser;
+import com.alibaba.druid.sql.test.TestUtils;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class OracleFlashbackQueryTest2 extends TestCase {
+
+    public void test_isEmpty() throws Exception {
+        String sql = "SELECT salary FROM employees\n"
+                     + "VERSIONS BETWEEN TIMESTAMP SYSTIMESTAMP - INTERVAL '10' MINUTE AND SYSTIMESTAMP - INTERVAL '1' MINUTE\n"
+                     + "WHERE last_name = 'Chung';";
+
+        String expect = "SELECT salary\n"
+                        + "FROM employees\n"
+                        + "VERSIONS BETWEEN TIMESTAMP SYSTIMESTAMP - INTERVAL '10' MINUTE AND SYSTIMESTAMP - INTERVAL '1' MINUTE\n"
+                        + "WHERE last_name = 'Chung';\n";
+        
+        OracleStatementParser parser = new OracleStatementParser(sql);
+        SQLSelectStatement stmt = (SQLSelectStatement) parser.parseStatementList().get(0);
+
+        String text = TestUtils.output(stmt);
+
+        Assert.assertEquals(expect, text);
+
+        System.out.println(text);
+    }
+}
