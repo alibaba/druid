cb18f00e93e617873be6c27f75cd9a1dd4770562
wenshao <szujobs@hotmail.com>
2011-10-14 07:31:12 +0000

    增强测试
    git-svn-id: http://code.alibabatech.com/svn/druid/trunk@961 b9813039-fb51-4c41-a8b9-e21c2acb5095

---
+++ src/test/java/com/alibaba/druid/bvt/sql/oracle/OracleFlashbackQueryTest.java
@@ -0,0 +1,29 @@
+package com.alibaba.druid.bvt.sql.oracle;
+
+import com.alibaba.druid.sql.ast.statement.SQLSelectStatement;
+import com.alibaba.druid.sql.dialect.oracle.parser.OracleStatementParser;
+import com.alibaba.druid.sql.test.TestUtils;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class OracleFlashbackQueryTest extends TestCase {
+
+	public void test_isEmpty() throws Exception {
+		String sql = "SELECT salary FROM employees AS OF TIMESTAMP (SYSTIMESTAMP - INTERVAL '1' DAY) WHERE last_name = 'Chung';";
+
+		String expect = "SELECT salary\n" + "FROM employees\n"
+				+ "AS OF TIMESTAMP (SYSTIMESTAMP - INTERVAL '1' DAY)\n"
+				+ "WHERE last_name = 'Chung';\n";
+
+		OracleStatementParser parser = new OracleStatementParser(sql);
+		SQLSelectStatement stmt = (SQLSelectStatement) parser
+				.parseStatementList().get(0);
+
+		String text = TestUtils.output(stmt);
+
+		Assert.assertEquals(expect, text);
+
+		System.out.println(text);
+	}
+}
