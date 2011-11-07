ee53327ccf7a811d2712d875a787083509462765
wenshao <szujobs@qq.com>
2011-11-07 15:54:40 +0800

    test merge large sql

---
+++ src/test/java/com/alibaba/druid/bvt/sql/mysql/MySqlParameterizedOutputVisitorTest2.java
@@ -0,0 +1,55 @@
+package com.alibaba.druid.bvt.sql.mysql;
+
+import java.util.List;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import com.alibaba.druid.sql.ast.SQLStatement;
+import com.alibaba.druid.sql.dialect.mysql.parser.MySqlStatementParser;
+import com.alibaba.druid.sql.dialect.mysql.visitor.MySqlParameterizedOutputVisitor;
+import com.alibaba.druid.sql.dialect.oracle.parser.OracleStatementParser;
+import com.alibaba.druid.sql.dialect.oracle.visitor.OracleParameterizedOutputVisitor;
+
+public class MySqlParameterizedOutputVisitorTest2 extends TestCase {
+
+    public void test_0() throws Exception {
+    	String sql = "SELECT * FROM T WHERE ID = ?";
+    	for (int i = 0; i < 10000; ++i) {
+    		sql += " OR ID = ?";
+    	}
+    	
+        validate(sql, "SELECT * FROM T WHERE ID = ?");
+        validateOracle(sql, "SELECT * FROM T WHERE ID = ?; ");
+    }
+
+    void validate(String sql, String expect) {
+
+        MySqlStatementParser parser = new MySqlStatementParser(sql);
+        List<SQLStatement> statementList = parser.parseStatementList();
+        SQLStatement statemen = statementList.get(0);
+
+        Assert.assertEquals(1, statementList.size());
+
+        StringBuilder out = new StringBuilder();
+        MySqlParameterizedOutputVisitor visitor = new MySqlParameterizedOutputVisitor(out);
+        statemen.accept(visitor);
+
+        Assert.assertEquals(expect, out.toString());
+    }
+
+    void validateOracle(String sql, String expect) {
+
+        OracleStatementParser parser = new OracleStatementParser(sql);
+        List<SQLStatement> statementList = parser.parseStatementList();
+        SQLStatement statemen = statementList.get(0);
+
+        Assert.assertEquals(1, statementList.size());
+
+        StringBuilder out = new StringBuilder();
+        OracleParameterizedOutputVisitor visitor = new OracleParameterizedOutputVisitor(out);
+        statemen.accept(visitor);
+
+        Assert.assertEquals(expect, out.toString());
+    }
+}
