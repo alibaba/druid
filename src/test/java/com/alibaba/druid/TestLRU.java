2b14fa2b4824d5c179326c1cf0fb573cbc63c266
wenshao <szujobs@qq.com>
2011-11-07 15:55:03 +0800

    test lru

---
+++ src/test/java/com/alibaba/druid/TestLRU.java
@@ -0,0 +1,22 @@
+package com.alibaba.druid;
+
+import java.util.LinkedHashMap;
+
+import junit.framework.TestCase;
+
+
+public class TestLRU extends TestCase {
+    public void test_lru() throws Exception {
+        LinkedHashMap<Integer, Object> cache = new LinkedHashMap<Integer, Object>(100, 0.75f, true);
+        
+        cache.put(2, "22");
+        cache.put(3, "33");
+        
+        System.out.println(cache);
+        
+        cache.put(2, "22");
+        
+        System.out.println(cache);
+        
+    }
+}
