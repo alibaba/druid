ded9087df0079aa8ee4e93dca4799f807d8ae3b4
温少 <szujobs@hotmail.com>
2012-02-25 18:51:37 +0800

    add SQLServerSchemaStatVisitor

---
+++ src/main/java/com/alibaba/druid/sql/dialect/sqlserver/visitor/SQLServerSchemaStatVisitor.java
@@ -0,0 +1,31 @@
+package com.alibaba.druid.sql.dialect.sqlserver.visitor;
+
+import com.alibaba.druid.sql.ast.statement.SQLSelectQueryBlock;
+import com.alibaba.druid.sql.dialect.sqlserver.ast.SQLServerSelectQueryBlock;
+import com.alibaba.druid.sql.dialect.sqlserver.ast.Top;
+import com.alibaba.druid.sql.visitor.SchemaStatVisitor;
+
+
+public class SQLServerSchemaStatVisitor extends SchemaStatVisitor implements SQLServerASTVisitor {
+
+    @Override
+    public boolean visit(SQLServerSelectQueryBlock x) {
+        return visit((SQLSelectQueryBlock) x);
+    }
+
+    @Override
+    public void endVisit(SQLServerSelectQueryBlock x) {
+        endVisit((SQLSelectQueryBlock) x);
+    }
+
+    @Override
+    public boolean visit(Top x) {
+        return false;
+    }
+
+    @Override
+    public void endVisit(Top x) {
+        
+    }
+
+}
