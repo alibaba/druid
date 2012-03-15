4bf0785f57087f754e1468614d5b5b350e579ab8
温少 <szujobs@hotmail.com>
2012-03-15 14:09:16 +0800

    druid 0.1.18 compatible

---
+++ src/main/java/com/alibaba/druid/util/TransactionInfo.java
@@ -0,0 +1,10 @@
+package com.alibaba.druid.util;
+
+@Deprecated
+public class TransactionInfo extends com.alibaba.druid.proxy.jdbc.TransactionInfo {
+
+    public TransactionInfo(long id){
+        super(id);
+    }
+
+}
