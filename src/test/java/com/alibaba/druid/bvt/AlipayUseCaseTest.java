aa03deaae521e656e05a45cdef386971c7fc0318
xiusiyan <xiusiyan@gmail.com>
2012-03-15 14:39:04 +0800

    新增Alipay使用druid测试用例。

---
+++ src/test/java/com/alibaba/druid/bvt/AlipayUseCaseTest.java
@@ -0,0 +1,37 @@
+package com.alibaba.druid.bvt;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * AlipayUseCaseTest
+ *
+ * @author xiusiyan
+ * @version 1.0, Mar 15, 2012
+ * @see
+ */
+public class AlipayUseCaseTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
