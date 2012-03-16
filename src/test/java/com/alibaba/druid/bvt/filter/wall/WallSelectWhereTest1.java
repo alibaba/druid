e6699763c601fb2c0d3c9083035eefce476d1285
温少 <szujobs@hotmail.com>
2012-03-16 15:21:28 +0800

    add test

---
+++ src/test/java/com/alibaba/druid/bvt/filter/wall/WallSelectWhereTest1.java
@@ -0,0 +1,24 @@
+package com.alibaba.druid.bvt.filter.wall;
+
+import junit.framework.TestCase;
+
+import org.junit.Assert;
+
+import com.alibaba.druid.filter.wall.WallUtils;
+
+/**
+ * 这个场景，检测可疑的Having条件
+ * @author wenshao
+ *
+ */
+public class WallSelectWhereTest1 extends TestCase {
+    private String sql = "SELECT F1, F2 WHERE 1 = 1 OR FID = ?";
+
+    public void testMySql() throws Exception {
+        Assert.assertFalse(WallUtils.isValidateMySql(sql));
+    }
+    
+    public void testORACLE() throws Exception {
+        Assert.assertFalse(WallUtils.isValidateOracle(sql));
+    }
+}
