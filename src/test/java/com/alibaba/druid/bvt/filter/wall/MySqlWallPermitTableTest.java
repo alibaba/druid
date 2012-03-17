7806c9589f013ea8b329bd4c1e2201de06b2638c
温少 <szujobs@hotmail.com>
2012-03-17 19:03:20 +0800

    bug fixed for sql wall

--- src/test/java/com/alibaba/druid/bvt/filter/wall/MySqlWallPermitTableTest.java
+++ src/test/java/com/alibaba/druid/bvt/filter/wall/MySqlWallPermitTableTest.java
@@ -2,6 +2,7 @@ package com.alibaba.druid.bvt.filter.wall;
 
 import org.junit.Assert;
 
+import com.alibaba.druid.filter.wall.WallConfig;
 import com.alibaba.druid.filter.wall.WallUtils;
 
 import junit.framework.TestCase;
@@ -13,8 +14,15 @@ import junit.framework.TestCase;
  */
 public class MySqlWallPermitTableTest extends TestCase {
 
-    public void test_permitTable() throws Exception {
+    public void test_default() throws Exception {
         Assert.assertFalse(WallUtils.isValidateMySql("select benchmark( 500000, sha1( 'test' ) )"));
+    }
+    
+    public void test_allow() throws Exception {
+        WallConfig config = new WallConfig();
+        config.setTableCheck(false);
+        
+        Assert.assertTrue(WallUtils.isValidateMySql("select benchmark( 500000, sha1( 'test' ) )", config));
     }
     
 }
