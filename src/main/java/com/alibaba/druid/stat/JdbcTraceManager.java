00ebf15a880198c6d6800f1d4c9b6456e2d807a9
温少 <szujobs@hotmail.com>
2012-03-19 22:17:09 +0800

    remove trace filter

--- src/main/java/com/alibaba/druid/stat/JdbcTraceManager.java
+++ src/main/java/com/alibaba/druid/stat/JdbcTraceManager.java
@@ -15,109 +15,12 @@
  */
 package com.alibaba.druid.stat;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
+public class JdbcTraceManager implements JdbcTraceManagerMBean {
 
-import javax.management.Notification;
-import javax.management.NotificationBroadcasterSupport;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-import com.alibaba.druid.filter.trace.TraceEvent;
-import com.alibaba.druid.filter.trace.TraceEventListener;
-
-public class JdbcTraceManager extends NotificationBroadcasterSupport implements JdbcTraceManagerMBean {
-
-    public final static Log                      LOG                  = LogFactory.getLog(JdbcTraceManager.class);
-
-    private boolean                              traceEnable          = false;
-
-    private final static JdbcTraceManager        instance             = new JdbcTraceManager();
-
-    private final List<TraceEventListener>       eventListeners       = new ArrayList<TraceEventListener>();
-
-    private final AtomicLong                     fireEventCount       = new AtomicLong();
-    private final AtomicLong                     skipEventCount       = new AtomicLong();
-
-    private boolean                              notificationEnable   = false;
-    private final NotificationTraceEventListener notificationListener = new NotificationTraceEventListener();
-
-    public boolean isNotificationEnable() {
-        return notificationEnable;
-    }
-
-    public void setNotificationEnable(boolean notificationEnable) {
-        this.notificationEnable = notificationEnable;
-    }
-
-    public NotificationTraceEventListener getNotificationListener() {
-        return notificationListener;
-    }
+    private final static JdbcTraceManager instance = new JdbcTraceManager();
 
     public static JdbcTraceManager getInstance() {
         return instance;
     }
 
-    public boolean isTraceEnable() {
-        return traceEnable;
-    }
-
-    public void setTraceEnable(boolean traceEnable) {
-        this.traceEnable = traceEnable;
-    }
-
-    public long getEventFiredCount() {
-        return fireEventCount.get();
-    }
-
-    public int getEventListenerSize() {
-        return this.eventListeners.size();
-    }
-
-    public long getEventSkipCount() {
-        return skipEventCount.get();
-    }
-
-    public void fireTraceEvent(TraceEvent event) {
-        fireEventCount.incrementAndGet();
-
-        if (!this.traceEnable) {
-            skipEventCount.incrementAndGet();
-            return;
-        }
-
-        if (isNotificationEnable()) {
-            notificationListener.fireEvent(event);
-        }
-
-        for (TraceEventListener listener : this.eventListeners) {
-            try {
-                listener.fireEvent(event);
-            } catch (Exception e) {
-                LOG.error("fireTraceEventError", e);
-            }
-        }
-    }
-
-    public List<TraceEventListener> getEventListeners() {
-        return eventListeners;
-    }
-
-    public class NotificationTraceEventListener implements TraceEventListener {
-
-        private long sequenceNumber = 0;
-
-        @Override
-        public void fireEvent(TraceEvent event) {
-            Notification notification = new Notification(event.getEventType(), JdbcTraceManager.class.getName(),
-                                                         sequenceNumber++);
-            notification.setTimeStamp(event.getEventTime().getTime());
-            notification.setUserData(event.getContext());
-
-            sendNotification(notification);
-        }
-
-    }
 }
