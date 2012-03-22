5324c2a0acf913a0b22e6d6c90f39d8296fc51f3
温少 <szujobs@hotmail.com>
2012-03-23 01:00:35 +0800

    0.2.1-SNAPSHOT

--- src/main/java/com/alibaba/druid/VERSION.java
+++ src/main/java/com/alibaba/druid/VERSION.java
@@ -19,5 +19,5 @@ public interface VERSION {
 
     public final static int MajorVersion    = 0;
     public final static int MinorVersion    = 2;
-    public final static int RevisionVersion = 0;
+    public final static int RevisionVersion = 1;
 }
