28abacd0ed7e2564a265a3e76f0c7ec90beed0df
温少 <szujobs@hotmail.com>
2012-05-20 00:47:26 +0800

    hbase support

---
+++ src/main/java/com/alibaba/druid/hbase/hbql/ast/HCreateTableStatement.java
@@ -0,0 +1,9 @@
+package com.alibaba.druid.hbase.hbql.ast;
+
+import com.alibaba.druid.sql.ast.statement.SQLCreateTableStatement;
+
+public class HCreateTableStatement extends SQLCreateTableStatement {
+
+    private static final long serialVersionUID = 1L;
+
+}
