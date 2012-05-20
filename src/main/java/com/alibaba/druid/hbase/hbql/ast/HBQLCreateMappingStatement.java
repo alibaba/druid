7dbce92bfbb1d1a7d96806a61381a7f63c065555
温少 <szujobs@hotmail.com>
2012-05-20 13:35:23 +0800

    refactor

--- src/main/java/com/alibaba/druid/hbase/hbql/ast/HBQLCreateMappingStatement.java
+++ src/main/java/com/alibaba/druid/hbase/hbql/ast/HBQLCreateMappingStatement.java
@@ -1,16 +1,12 @@
 package com.alibaba.druid.hbase.hbql.ast;
 
-<<<<<<< HEAD
 import com.alibaba.druid.hbase.mapping.HMapping;
-=======
->>>>>>> ca778ea3c3ae05528c08c708193c8a4f79392825
 import com.alibaba.druid.sql.ast.SQLStatementImpl;
 
 public class HBQLCreateMappingStatement extends SQLStatementImpl implements HBQLStatement {
 
     private static final long serialVersionUID = 1L;
 
-<<<<<<< HEAD
     private HMapping          mapping          = new HMapping();
 
     public HMapping getMapping() {
@@ -21,6 +17,4 @@ public class HBQLCreateMappingStatement extends SQLStatementImpl implements HBQL
         this.mapping = mapping;
     }
 
-=======
->>>>>>> ca778ea3c3ae05528c08c708193c8a4f79392825
 }
