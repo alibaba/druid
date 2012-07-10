97b85470b9e895cf5c36b72b143e63f420ad75e2
wenshao <szujobs@hotmail.com>
2012-07-11 06:01:04 +0800

    fixed benchmark testcase

--- src/test/java/com/alibaba/druid/benckmark/pool/Case0.java
+++ src/test/java/com/alibaba/druid/benckmark/pool/Case0.java
@@ -46,17 +46,17 @@ public class Case0 extends TestCase {
 
     private long     minEvictableIdleTimeMillis = 3000;
     public final int LOOP_COUNT                 = 5;
-    public final int COUNT                      = 1000 * 1;
+    public final int COUNT                      = 1000 * 1000;
 
     protected void setUp() throws Exception {
-        // jdbcUrl = "jdbc:fake:dragoon_v25masterdb";
-        // user = "dragoon25";
-        // password = "dragoon25";
-        // driverClass = "com.alibaba.druid.mock.MockDriver";
+         jdbcUrl = "jdbc:fake:dragoon_v25masterdb";
+         user = "dragoon25";
+         password = "dragoon25";
+         driverClass = "com.alibaba.druid.mock.MockDriver";
 
-        jdbcUrl = "jdbc:mysql://10.20.153.104:3306/druid2";
-        user = "root";
-        password = "root";
+//        jdbcUrl = "jdbc:mysql://10.20.153.104:3306/druid2";
+//        user = "root";
+//        password = "root";
     }
 
     public void test_0() throws Exception {
