36c2851e9c0117176d65582b22079abe639884ef
wenshao <szujobs@hotmail.com>
2012-07-20 10:11:12 +0800

    sql server driver testcase

--- src/test/java/com/alibaba/druid/bvt/bug/Bug_for_JeffYin.java
+++ src/test/java/com/alibaba/druid/bvt/bug/Bug_for_JeffYin.java
@@ -1,5 +1,8 @@
 package com.alibaba.druid.bvt.bug;
 
+import java.sql.Driver;
+
+import junit.framework.Assert;
 import junit.framework.TestCase;
 
 import com.alibaba.druid.util.JdbcUtils;
@@ -7,6 +10,13 @@ import com.alibaba.druid.util.JdbcUtils;
 public class Bug_for_JeffYin extends TestCase {
 
     public void test_0() throws Exception {
-        String driverClass = JdbcUtils.getDriverClassName("jdbc:sqlserver://localhost:1433;");
+        String url = "jdbc:sqlserver://localhost:1433;";
+        String driverClassName = JdbcUtils.getDriverClassName(url);
+        Class<?> driverClass = JdbcUtils.loadDriverClass(driverClassName);
+        Assert.assertNotNull(driverClass);
+        
+        Driver driver = (Driver) driverClass.newInstance();
+        
+        Assert.assertTrue(driver.acceptsURL(url));
     }
 }
