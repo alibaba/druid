8377018fe64068727d199d2cfb92632ea7f3fbe4
wenshao <szujobs@hotmail.com>
2012-07-20 10:41:45 +0800

    change benchmark testcase

--- src/test/java/com/alibaba/druid/benckmark/pool/Case0.java
+++ src/test/java/com/alibaba/druid/benckmark/pool/Case0.java
@@ -49,14 +49,14 @@ public class Case0 extends TestCase {
     public final int COUNT                      = 1000 * 1000 * 1;
 
     protected void setUp() throws Exception {
-         jdbcUrl = "jdbc:fake:dragoon_v25masterdb";
-         user = "dragoon25";
-         password = "dragoon25";
-         driverClass = "com.alibaba.druid.mock.MockDriver";
+//         jdbcUrl = "jdbc:fake:dragoon_v25masterdb";
+//         user = "dragoon25";
+//         password = "dragoon25";
+//         driverClass = "com.alibaba.druid.mock.MockDriver";
 
-//        jdbcUrl = "jdbc:mysql://10.20.159.104:3306/masterdb";
-//        user = "dragoon";
-//        password = "dragoon";
+        jdbcUrl = "jdbc:mysql://10.20.159.104:3306/masterdb";
+        user = "dragoon";
+        password = "dragoon";
     }
 
     public void test_0() throws Exception {
@@ -134,10 +134,10 @@ public class Case0 extends TestCase {
 
         for (int i = 0; i < COUNT; ++i) {
             Connection conn = dataSource.getConnection();
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT 1");
-            rs.close();
-            stmt.close();
+//            Statement stmt = conn.createStatement();
+//            ResultSet rs = stmt.executeQuery("SELECT 1");
+//            rs.close();
+//            stmt.close();
             conn.close();
         }
         long millis = System.currentTimeMillis() - startMillis;
