d17962d5c97727ee8a3bdf53c94109d80d39ce27
sand <sandzhangtoo@gmail.com>
2012-08-10 09:23:14 +0800

    add log

--- src/main/java/com/alibaba/druid/util/ReflectionUtils.java
+++ src/main/java/com/alibaba/druid/util/ReflectionUtils.java
@@ -4,89 +4,60 @@ import java.lang.reflect.Method;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.alibaba.druid.support.logging.Log;
+import com.alibaba.druid.support.logging.LogFactory;
+
 /**
  * @author sandzhang<sandzhangtoo@gmail.com>
  */
-public class ReflectionUtils
-{
+public class ReflectionUtils {
 
-    public static Class<?> getClassFromWebContainerOrCurrentClassLoader(String className)
-    {
+    private final static Log LOG = LogFactory.getLog(ReflectionUtils.class);
+
+    public static Class<?> getClassFromWebContainerOrCurrentClassLoader(String className) {
         Class<?> result = null;
-        try
-        {
+        try {
             result = HttpServletRequest.class.getClassLoader().loadClass(className);
+        } catch (ClassNotFoundException e) {
+            LOG.debug("can'r find class in web container classLoader ", e);
         }
-        catch (ClassNotFoundException e)
-        {
-        }
-        if (result == null)
-        {
-            try
-            {
+        if (result == null) {
+            try {
                 result = Thread.currentThread().getContextClassLoader().loadClass(className);
-            }
-            catch (ClassNotFoundException e)
-            {
+            } catch (ClassNotFoundException e) {
+                LOG.error("can'r find class in current thread context classLoader ", e);
             }
         }
         return result;
     }
 
+    public static Object callStaticMethod(Class<?> classObject, String methodName) {
 
-    public static Object callStaticMethod(Class<?> classObject, String methodName)
-    {
-
-        try
-        {
+        try {
             Method m = classObject.getMethod(methodName);
             return m.invoke(classObject);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
+            LOG.warn("callStaticMethod fail:class=" + classObject.getName() + " method=" + methodName, e);
             return null;
         }
     }
 
-
-    public static Object callObjectMethod(Object obj, String methodName)
-    {
-
-        try
-        {
+    public static Object callObjectMethod(Object obj, String methodName, Object... methodParams) {
+        try {
             Method m = obj.getClass().getMethod(methodName);
-            return m.invoke(obj);
-        }
-        catch (Exception e)
-        {
+            return m.invoke(obj, methodParams);
+        } catch (Exception e) {
+            LOG.warn("callObjectMethod fail:class=" + obj.getClass().getName() + " method=" + methodName, e);
             return null;
         }
     }
 
-
-    public static Object callObjectMethod(Object obj, String methodName, Object methodParam)
-    {
-        try
-        {
-            Method m = obj.getClass().getMethod(methodName);
-            return m.invoke(obj,methodParam);
-        }
-        catch (Exception e)
-        {
-            return null;
-        }
-    }
-
-
-    public void getTest()
-    {
+    public void getTest() {
         System.out.println("dd");
         // return "test";
     }
 
-
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         System.out.println(callObjectMethod(new ReflectionUtils(), "getTest"));
 
     }
