f26c98ace99e1c5681e92153c345ad83b19c9f5a
wenshao <szujobs@hotmail.com>
2012-08-12 19:41:12 +0800

    add WebSessionStat support

---
+++ src/main/java/com/alibaba/druid/util/DruidWebUtils.java
@@ -0,0 +1,21 @@
+package com.alibaba.druid.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class DruidWebUtils {
+
+    public static String getRemoteAddr(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+
+        return ip;
+    }
+}
