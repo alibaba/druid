0e6e10c27cfe97c8477d3f2af8d68bc7a0327423
wenshao <szujobs@hotmail.com>
2012-08-18 17:35:16 +0800

    improve WebSessionStat

--- src/main/resources/support/http/resources/js/websession-detail.js
+++ src/main/resources/support/http/resources/js/websession-detail.js
@@ -19,7 +19,6 @@ function ajaxResponseForSqlInfo() {
 	changeInnerHtml("RemoteAddress", stat.RemoteAddress);
 	changeInnerHtml("RequestCount", stat.RequestCount);
 	changeInnerHtml("RequestTimeMillisTotal", stat.RequestTimeMillisTotal);
-	changeInnerHtml("RequestInterval", '[' + stat.RequestInterval + ']');
 	changeInnerHtml("RunningCount", stat.RunningCount);
 	changeInnerHtml("ConcurrentMax", stat.ConcurrentMax);
 	changeInnerHtml("JdbcExecuteCount", stat.JdbcExecuteCount);
@@ -28,4 +27,20 @@ function ajaxResponseForSqlInfo() {
 	changeInnerHtml("JdbcRollbackCount", stat.JdbcRollbackCount);
 	changeInnerHtml("JdbcFetchRowCount", stat.JdbcFetchRowCount);
 	changeInnerHtml("JdbcUpdateCount", stat.JdbcUpdateCount);
+	
+	{
+		var html = '[';
+		html += '<a title="count of < 1 ms">' + stat.RequestInterval[0] + '</a>';
+		html += ', <a title="count of 1 - 10 ms">' + stat.RequestInterval[1] + '</a>';
+		html += ', <a title="count of 10 - 100 ms">' + stat.RequestInterval[2] + '</a>';
+		html += ', <a title="count of 100 - 1000 ms">' + stat.RequestInterval[3] + '</a>';
+		html += ', <a title="count of 1 - 10 s">' + stat.RequestInterval[4] + '</a>';
+		html += ', <a title="count of 10 - 100 s">' + stat.RequestInterval[5] + '</a>';
+		html += ', <a title="count of 100 - 1000 s">' + stat.RequestInterval[6] + '</a>';
+		html += ', <a title="count of 1000 - 10000 s">' + stat.RequestInterval[7] + '</a>';
+		html += ', <a title="count of > 10000 s">' + stat.RequestInterval[8] + '</a>';
+		html += ']';
+		
+		changeInnerHtml("RequestInterval", html);
+	}
 }
