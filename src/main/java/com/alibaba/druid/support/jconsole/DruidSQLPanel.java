733cf5ef8b95297e994122297f955547a0309fe1
wenshao <szujobs@hotmail.com>
2012-08-21 16:09:18 +0800

    add jconsole support

--- src/main/java/com/alibaba/druid/support/jconsole/DruidSQLPanel.java
+++ src/main/java/com/alibaba/druid/support/jconsole/DruidSQLPanel.java
@@ -1,8 +1,19 @@
 package com.alibaba.druid.support.jconsole;
 
+import javax.management.MBeanServerConnection;
+import javax.management.ObjectName;
+
+import com.alibaba.druid.stat.DruidStatService;
 
 public class DruidSQLPanel extends DruidPanel {
 
     private static final long serialVersionUID = 1L;
 
+    protected void doInBackground(MBeanServerConnection conn) throws Exception {
+        ObjectName name = new ObjectName(DruidStatService.MBEAN_NAME);
+        String url = "/sql.json";
+        String result = (String) conn.invoke(name, "service", new String[] { url }, new String[] { String.class.getName() });
+
+        System.out.println("doInBackground");
+    }
 }
