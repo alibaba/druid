74151fedfd77d7fd88980918aa945a8035be06d8
wenshao <szujobs@hotmail.com>
2012-08-24 19:02:34 +0800

    bug fixed for sql parser

--- src/main/java/com/alibaba/druid/sql/ast/SQLOrderBy.java
+++ src/main/java/com/alibaba/druid/sql/ast/SQLOrderBy.java
@@ -19,7 +19,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.alibaba.druid.sql.ast.statement.SQLSelectOrderByItem;
-import com.alibaba.druid.sql.dialect.oracle.ast.stmt.OracleOrderByItem;
 import com.alibaba.druid.sql.visitor.SQLASTVisitor;
 
 @SuppressWarnings("serial")
@@ -52,7 +51,7 @@ public class SQLOrderBy extends SQLObjectImpl {
             if (i != 0) {
                 buf.append(", ");
             }
-            ((OracleOrderByItem) this.items.get(i)).output(buf);
+            this.items.get(i).output(buf);
         }
     }
 }
