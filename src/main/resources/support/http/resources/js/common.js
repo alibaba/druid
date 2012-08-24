6c928358acedca867e84857534f485077ab125d3
melin <libinsong1204@gmail.com>
2012-08-24 09:10:14 +0800

    add jquery

--- src/main/resources/support/http/resources/js/common.js
+++ src/main/resources/support/http/resources/js/common.js
@@ -1,66 +1,75 @@
-function changeInnerHtml(id, divHtml) {
-	var divObj = document.getElementById(id);
-	if (divHtml == undefined)
-		divHtml = '';
-	if (divObj) {
-		divObj.innerHTML = divHtml;
-	}
-}
+$.namespace("druid.common");
+druid.common = function () {  
+	return  {
+		init : function() {
+			this.buildHead();
+			this.buildMenu();
+			this.buildFooter();
+		},
+		
+		buildHead : function() {
+			var html = '<div class="navbar navbar-fixed-top">'+
+				       '	<div class="navbar-inner">'+
+				       ' 		<div class="container-fluid">'+
+				       '   			<a class="brand" href="#">Druid Monitor</a>'+
+				       '   			<div class="pull-right">'+
+				       '   				<a href="javascript:druid.common.ajaxRequestForReset()" class="btn">Reset All</a>'+
+				       '   			</div>'+
+				       ' 		</div>'+
+				       '		</div>'+
+				       '</div>';
+			$(document.body).prepend(html);
+		},
+		
+		buildMenu : function() {
+			var html = '<div class="span2">'+
+				      	'		<div class="well sidebar-nav">'+
+						'	<ul class="nav nav-list">'+
+				  		'		<li class="nav-header"><h3>Menu</h3></li>'+
+				  		'		<li id="configId"><a href="index.html">Index</a></li>'+
+				  		'		<li id="userAddId"><a href="datasource.html">DataSource</a></li>'+
+				  		'		<li id="userAddId"><a href="sql.html">Sql</a></li>'+
+				  		'		<li id="userAddId"><a href="weburi.html">WebURI</a></li>'+
+				  		'		<li id="userAddId"><a href="websession.html">Web Session</a></li>'+
+				  		'		<li id="userAddId"><a href="spring.html">Spring</a></li>'+
+				  		'		<li id="userAddId"><a href="api.html">JSON API</a></li>'+
+						'	</ul>'+
+						'	</div>'+
+					'</div>';
+			$(".row-fluid").prepend(html);
+		},
 
-function checkResoponse(xmlHttpForRequest){
-	if (xmlHttpForRequest.readyState != 4) {
-		return false;
-	}
-	if (xmlHttpForRequest.status != 200) {
-		return false;
-	}
-	return true;
-}
-
-function getJSONResponseContent(xmlHttpForRequest){
-	if(checkResoponse(xmlHttpForRequest)){
-		var jsonResp = eval("(" + xmlHttpForRequest.responseText + ")");
-		if (jsonResp.ResultCode == 1) {
-			return jsonResp.Content;
+		buildFooter : function() {
+			var html = '<footer class="footer">'+
+					  '    		<div class="container">'+
+				  		'	powered by <a href="mailto:sandzhangtoo@gmail.com">sandzhang</a> & <a href="mailto:libinsong1204@gmail.com">melin</a>'+
+				  	'	</div>'+
+					'</footer>';
+			$(document.body).append(html);
+		},
+		
+		ajaxRequestForReset : function() {
+			if(!confirm("Are you sure to reset all stat? It'll clear all stat data !")){
+				return;
+			}
+			
+			$.ajax({
+				type: 'POST',
+				url: "reset-all.json",
+				success: function(data) {
+					if (data.ResultCode == 1) {
+						alert("already reset all stat");
+					}
+				},
+				dataType: "json"
+			});
 		}
 	}
-	return null;
-}
+}();
 
-function sendRequest(requestObject,url,callBack){
-	requestObject.onreadystatechange = callBack;
-	requestObject.open("GET", url, true);
-	requestObject.send(null);
-}
-
-function getRequestObject(){
-	var requestObject;
-	if (window.XMLHttpRequest)
-		requestObject = new XMLHttpRequest();
-	else if (window.ActiveXObject)
-		requestObject = new ActiveXObject("Microsoft.XMLHTTP");
-	return requestObject;
-}
-
-
-function ajaxRequestForReset() {
-	if(!confirm("Are you sure to reset all stat? It'll clear all stat data !")){
-		return;
-	}
-	xmlHttpForReset =  getRequestObject();
-	sendRequest(xmlHttpForReset,'reset-all.json',ajaxResponseForReset)
-}
-
-function ajaxResponseForReset() {
-	if (!checkResoponse(xmlHttpForReset)) {
-		return;
-	}
-	var jsonResp = eval("(" + xmlHttpForReset.responseText + ")");
-	if (jsonResp.ResultCode == 1) {
-		alert("already reset all stat");
-	}
-
-}
+$(document).ready(function() {
+	druid.common.init();
+});
 
 function getUrlVar(name) {
     var vars = {};
@@ -68,31 +77,4 @@ function getUrlVar(name) {
         vars[key] = value;
     });
 		return vars[name];
-}
-
-function $(id) {
-	return document.getElementById(id);
-}
-
-function buildMenu() {
-  var divEle = document.createElement("div"); 
-	divEle.id = "menu"; 
-	divEle.innerHTML = " <a href='index.html'>Index</a> |  " 
-      + "<a href='datasource.html'>DataSource</a> | " 
-      + "<a href='sql.html'>Sql</a> | "
-      + "<a href='weburi.html'>WebURI</a> | "
-      + "<a href='websession.html'>Web Session</a> | "
-      + "<a href='spring.html'>Spring</a> | "
-      + "<a href='api.html'>JSON API</a> "
-      + "<a href='#' style='display:block;margin-right:20px;float:right' onclick='ajaxRequestForReset()'>Reset All</a> ";
-
-	document.body.insertBefore(divEle, document.body.firstChild); 
-	document.body.insertBefore(document.createElement("br"), document.body.firstChild); 
-}
-
-function buildFooter() {
-	var divEle = document.createElement("div"); 
-	divEle.id = "footer"; 
-	divEle.innerHTML = " powered by <a href='mailto:sandzhangtoo@gmail.com'>sandzhang</a>" ;
-	document.body.appendChild(divEle);
 }
