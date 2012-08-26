8a852409aa5b38c631b09d0a661bcc90ec29d59d
白一梓 <>
2012-08-26 16:28:12 +0800

    增加了jconsole监控界面

--- src/main/java/com/alibaba/druid/support/jconsole/DruidDriverPanel.java
+++ src/main/java/com/alibaba/druid/support/jconsole/DruidDriverPanel.java
@@ -15,8 +15,37 @@
  */
 package com.alibaba.druid.support.jconsole;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+import com.alibaba.druid.support.jconsole.model.DruidTableModel;
+
+/**
+ * 请求地址：/basic.json
+ * 
+ * 请求返回的json格式：
+ * {"ResultCode":1,"Content":
+ * 			{"Version":"0.2.6","Drivers":
+ * 					["sun.jdbc.odbc.JdbcOdbcDriver","com.alibaba.druid.mock.MockDriver",
+ * 					"com.mysql.jdbc.Driver","com.alibaba.druid.proxy.DruidDriver"]
+ * 			}
+ * }
+ * */
 public class DruidDriverPanel extends DruidPanel {
 
     private static final long serialVersionUID = 1L;
+    private static final String REQUEST_URL = "/basic.json";
+    
+    public DruidDriverPanel() {
+    	super();
+    	url = REQUEST_URL;
+    }
 
+	@Override
+	protected void tableDataProcess(
+			ArrayList<LinkedHashMap<String, Object>> data) {
+		tableModel = new DruidTableModel(data);
+		table.setModel(tableModel);
+	}    
 }
+
--- src/main/java/com/alibaba/druid/support/jconsole/DruidSQLPanel.java
+++ src/main/java/com/alibaba/druid/support/jconsole/DruidSQLPanel.java
@@ -15,22 +15,95 @@
  */
 package com.alibaba.druid.support.jconsole;
 
-import javax.management.MBeanServerConnection;
-import javax.management.ObjectName;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
-import com.alibaba.druid.stat.DruidStatService;
-import com.alibaba.druid.support.json.JSONUtils;
-
+import com.alibaba.druid.support.jconsole.model.DruidTableModel;
+import com.alibaba.druid.support.jconsole.model.RowHeaderTable;
+/**
+ * 请求地址 /sql.json
+ * 
+ * 返回数据格式：
+ * <pre>
+ * {
+    "ResultCode": 1,
+    "Content": [
+        {
+            "BatchSizeMax": 0,
+            "BatchSizeTotal": 0,
+            "ConcurrentMax": 1,
+            "DbType": "h2",
+            "EffectedRowCount": 18,
+            "EffectedRowCountHistogram": [
+                0,6,0,0,0,0
+            ],
+            "EffectedRowCountMax": 4,
+            "ErrorCount": 0,
+            "ExecuteAndResultHoldTimeHistogram": [
+                5,0,1,0,0,0,0,0
+            ],
+            "ExecuteAndResultSetHoldTime": 0,
+            "ExecuteCount": 6,
+            "FetchRowCount": 0,
+            "FetchRowCountHistogram": [
+                6,0,0,0,0,0
+            ],
+            "FetchRowCountMax": 0,
+            "Histogram": [
+                5,0,1,0,0,0,0,0
+            ],
+            "ID": 9,
+            "InTransactionCount": 6,
+            "LastSlowParameters": "[4]",
+            "LastTime": "2012-08-22 02:57:56",
+            "MaxTimespan": 18,
+            "MaxTimespanOccurTime": "2012-08-22 12:38:56",
+            "ResultSetHoldTime": 0,
+            "RunningCount": 0,
+            "SQL": "delete from acct_group_permission where group_id=?",
+            "TotalTime": 21
+        },
+     ]
+  }
+  </pre>
+ * */
 public class DruidSQLPanel extends DruidPanel {
 
     private static final long serialVersionUID = 1L;
+    private static final String REQUEST_URL = "/sql.json";
+    private static final ArrayList<String> SHOW_LIST = new ArrayList<String>() {
 
-    protected void doInBackground(MBeanServerConnection conn) throws Exception {
-        ObjectName name = new ObjectName(DruidStatService.MBEAN_NAME);
-        String url = "/sql.json";
-        String result = (String) conn.invoke(name, "service", new String[] { url }, new String[] { String.class.getName() });
-        Object o = JSONUtils.parse(result);
+		private static final long serialVersionUID = 1L;
 
-        System.out.println(o);
-    }
+		{
+    		add("SQL");
+    		add("ExecuteCount");
+    		add("MaxTimespan");
+    		add("InTransactionCount");
+    		add("ErrorCount");
+    		add("EffectedRowCount"); 
+    		add("FetchRowCount");
+    		add("RunningCount");
+    		add("ConcurrentMax");
+    		add("EffectedRowCountHistogram");
+    		add("ExecuteAndResultHoldTimeHistogram");
+    		add("FetchRowCountHistogram");
+    	}
+    };  
+    private RowHeaderTable header;
+
+	public DruidSQLPanel() {
+		super();
+		this.url = REQUEST_URL;
+	}
+
+	@Override
+	protected void tableDataProcess(
+			ArrayList<LinkedHashMap<String, Object>> data) {
+		tableModel = new DruidTableModel(data,SHOW_LIST);
+		table.setModel(tableModel);
+		
+		header = new RowHeaderTable(table,20);
+		scrollPane.setRowHeaderView(header);
+	}   
 }
