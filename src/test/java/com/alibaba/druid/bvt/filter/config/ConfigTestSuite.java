1201a525d147b2604555505204b756cb1b7c121a
wenshao <szujobs@hotmail.com>
2012-08-31 21:40:20 +0800

    revmoe failed testcase

--- src/test/java/com/alibaba/druid/bvt/filter/config/ConfigTestSuite.java
+++ src/test/java/com/alibaba/druid/bvt/filter/config/ConfigTestSuite.java
@@ -1,13 +1,13 @@
 package com.alibaba.druid.bvt.filter.config;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
 import com.alibaba.druid.filter.config.ConfigFilterTest;
 import com.alibaba.druid.filter.config.loader.ConfigLoaderFactoryTest;
 import com.alibaba.druid.filter.config.loader.impl.FileConfigLoaderTest;
 import com.alibaba.druid.filter.config.loader.impl.FileConfigLoaderTest1;
 import com.alibaba.druid.filter.config.loader.impl.HttpConfigLoaderTest;
-import com.alibaba.druid.filter.config.loader.impl.HttpConfigLoaderTest1;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
 
 /**
  * @author Jonas Yang
@@ -19,7 +19,6 @@ import org.junit.runners.Suite;
         FileConfigLoaderTest.class,
         FileConfigLoaderTest1.class,
         HttpConfigLoaderTest.class,
-        HttpConfigLoaderTest1.class,
         ConfigFilterTest.class
 })
 public class ConfigTestSuite {
