32e3ed98e621529f52e43979527258751f00851d
wenshao <szujobs@hotmail.com>
2013-05-26 15:55:08 +0800

    fixed testcase

--- src/test/java/com/alibaba/druid/bvt/sql/mysql/MySqlUpdateTest_1.java
+++ src/test/java/com/alibaba/druid/bvt/sql/mysql/MySqlUpdateTest_1.java
@@ -17,14 +17,13 @@ package com.alibaba.druid.bvt.sql.mysql;
 
 import java.util.List;
 
-import junit.framework.Assert;
+import org.junit.Assert;
 
 import com.alibaba.druid.sql.MysqlTest;
 import com.alibaba.druid.sql.ast.SQLStatement;
 import com.alibaba.druid.sql.dialect.mysql.parser.MySqlStatementParser;
 import com.alibaba.druid.sql.dialect.mysql.visitor.MySqlSchemaStatVisitor;
 import com.alibaba.druid.stat.TableStat;
-import com.alibaba.druid.stat.TableStat.Column;
 
 public class MySqlUpdateTest_1 extends MysqlTest {
 
