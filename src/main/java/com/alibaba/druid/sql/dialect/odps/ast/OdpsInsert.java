a26b3142d174f065983db8eb15faf3bcc46b927c
wenshao <szujobs@hotmail.com>
2013-12-24 11:57:42 +0800

    add license

--- src/main/java/com/alibaba/druid/sql/dialect/odps/ast/OdpsInsert.java
+++ src/main/java/com/alibaba/druid/sql/dialect/odps/ast/OdpsInsert.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 1999-2011 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alibaba.druid.sql.dialect.odps.ast;
 
 import java.util.ArrayList;
@@ -11,13 +26,13 @@ import com.alibaba.druid.sql.ast.statement.SQLSelect;
 import com.alibaba.druid.sql.dialect.odps.visitor.OdpsASTVisitor;
 import com.alibaba.druid.sql.visitor.SQLASTVisitor;
 
-
 public class OdpsInsert extends SQLObjectImpl {
-    private boolean                overwrite  = false;
-    private List<SQLAssignItem>    partitions = new ArrayList<SQLAssignItem>();
-    protected SQLExprTableSource   tableSource;
 
-    private SQLSelect              query;
+    private boolean              overwrite  = false;
+    private List<SQLAssignItem>  partitions = new ArrayList<SQLAssignItem>();
+    protected SQLExprTableSource tableSource;
+
+    private SQLSelect            query;
 
     public boolean isOverwrite() {
         return overwrite;
@@ -35,7 +50,6 @@ public class OdpsInsert extends SQLObjectImpl {
         this.partitions = partitions;
     }
 
-    
     public SQLExprTableSource getTableSource() {
         return tableSource;
     }
@@ -66,7 +80,7 @@ public class OdpsInsert extends SQLObjectImpl {
     protected void accept0(SQLASTVisitor visitor) {
         accept0((OdpsASTVisitor) visitor);
     }
-    
+
     protected void accept0(OdpsASTVisitor visitor) {
         if (visitor.visit(this)) {
             acceptChild(visitor, tableSource);
