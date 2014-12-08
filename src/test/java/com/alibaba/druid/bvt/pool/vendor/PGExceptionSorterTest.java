fbb27230e1dbaefa38c0ea6344470006f00407a2
wenshao <szujobs@hotmail.com>
2014-12-08 23:47:20 +0800

    improved postgresql support

---
+++ src/test/java/com/alibaba/druid/bvt/pool/vendor/PGExceptionSorterTest.java
@@ -0,0 +1,21 @@
+package com.alibaba.druid.bvt.pool.vendor;
+
+import org.junit.Assert;
+import org.postgresql.util.GT;
+import org.postgresql.util.PSQLException;
+import org.postgresql.util.PSQLState;
+
+import junit.framework.TestCase;
+
+import com.alibaba.druid.pool.vendor.PGExceptionSorter;
+
+public class PGExceptionSorterTest extends TestCase {
+
+    public void test_pg() throws Exception {
+        PGExceptionSorter exSorter = new PGExceptionSorter();
+
+        PSQLException ex = new PSQLException(GT.tr("Expected an EOF from server, got: {0}", new Integer(0)),
+                                             PSQLState.COMMUNICATION_ERROR);
+        Assert.assertTrue(exSorter.isExceptionFatal(ex));
+    }
+}
