bff2a6f9e6558dc25e2dcbcc74a755c8d5931755
bucketli <zylicfc@gmail.com>
2015-04-20 09:38:23 +0800

    fix unique bug

--- src/main/java/com/alibaba/druid/sql/parser/SQLExprParser.java
+++ src/main/java/com/alibaba/druid/sql/parser/SQLExprParser.java
@@ -1516,7 +1516,7 @@ public class SQLExprParser extends SQLParser {
             if (lexer.token() == Token.KEY) {
                 lexer.nextToken();
             }
-            column.getConstraints().add(new SQLColumnPrimaryKey());
+            column.getConstraints().add(new SQLColumnUniqueKey());
             return parseColumnRest(column);
         }
 
