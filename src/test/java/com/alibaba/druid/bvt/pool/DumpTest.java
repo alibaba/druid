883b1fbc640a15c1ff60aff6dd90675631dfebc3
高铁 <shaojin.wensj@alibaba-inc.com>
2015-08-16 23:45:03 +0800

    fixed copyright

--- src/test/java/com/alibaba/druid/bvt/pool/DumpTest.java
+++ src/test/java/com/alibaba/druid/bvt/pool/DumpTest.java
@@ -13,7 +13,7 @@ import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.stat.JdbcSqlStat;
 
 /*
- * Copyright 1999-2011 Alibaba Group Holding Ltd.
+ * Copyright 1999-2101 Alibaba Group Holding Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
