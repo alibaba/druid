a64b9f92875d897fa59abdfe481b6221540ab5c4
lijian <li_jian@gozap.com>
2015-08-18 16:53:36 +0800

    syn alibaba/druid/master

--- src/test/java/com/alibaba/druid/pool/MaxWaitTest.java
+++ src/test/java/com/alibaba/druid/pool/MaxWaitTest.java
@@ -19,6 +19,7 @@ import java.sql.Connection;
 import java.sql.Statement;
 import java.util.concurrent.CountDownLatch;
 
+import EDU.oswego.cs.dl.util.concurrent.CountDown;
 import junit.framework.TestCase;
 
 public class MaxWaitTest extends TestCase {
