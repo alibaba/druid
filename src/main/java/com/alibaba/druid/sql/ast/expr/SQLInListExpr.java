3cf7cdf59b9c46850cdc7620141a4da54f4b228d
高铁 <shaojin.wensj@alibaba-inc.com>
2016-02-17 16:42:26 +0800

    bug fixed for SQLInListExpr, issue 1076

--- src/main/java/com/alibaba/druid/sql/ast/expr/SQLInListExpr.java
+++ src/main/java/com/alibaba/druid/sql/ast/expr/SQLInListExpr.java
@@ -35,13 +35,11 @@ public class SQLInListExpr extends SQLExprImpl implements Serializable {
     }
 
     public SQLInListExpr(SQLExpr expr){
-
-        this.expr = expr;
+        this.setExpr(expr);
     }
 
     public SQLInListExpr(SQLExpr expr, boolean not){
-
-        this.expr = expr;
+        this.setExpr(expr);
         this.not = not;
     }
 
