190faee6d25ce96173f4cb6f2c30334dd7c574d7
wenshao <szujobs@hotmail.com>
2017-03-12 22:48:39 +0800

    1.2.29-SNAPSHOT

--- src/main/java/com/alibaba/druid/VERSION.java
+++ src/main/java/com/alibaba/druid/VERSION.java
@@ -19,7 +19,7 @@ public final class VERSION {
 
     public final static int MajorVersion    = 1;
     public final static int MinorVersion    = 0;
-    public final static int RevisionVersion = 28;
+    public final static int RevisionVersion = 29;
 
     public static String getVersionNumber() {
         return VERSION.MajorVersion + "." + VERSION.MinorVersion + "." + VERSION.RevisionVersion;
