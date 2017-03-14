c3db40c748ae078ea0de8ea27b8370a451cade3f
wenshao <szujobs@hotmail.com>
2017-03-14 21:18:18 +0800

    fixed copyright info. 2101 -> 2017

--- src/main/java/com/alibaba/druid/sql/ast/expr/SQLTimestampExpr.java
+++ src/main/java/com/alibaba/druid/sql/ast/expr/SQLTimestampExpr.java
@@ -5,7 +5,7 @@ import com.alibaba.druid.sql.ast.SQLExprImpl;
 import com.alibaba.druid.sql.visitor.SQLASTVisitor;
 
 /*
- * Copyright 1999-2101 Alibaba Group Holding Ltd.
+ * Copyright 1999-2017 Alibaba Group Holding Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
