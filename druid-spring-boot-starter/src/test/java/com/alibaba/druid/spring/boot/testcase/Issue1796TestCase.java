23596d10a823d8089dac5aeeccb4a53d5f8f8e50
lihengming <89921218@qq.com>
2017-07-23 20:41:46 +0800

    add > test case for V1.1.2

---
+++ druid-spring-boot-starter/src/test/java/com/alibaba/druid/spring/boot/testcase/Issue1796TestCase.java
@@ -0,0 +1,35 @@
+package com.alibaba.druid.spring.boot.testcase;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.spring.boot.demo.DemoApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author lihengming [89921218@qq.com]
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DemoApplication.class)
+@ActiveProfiles("issue-1796")
+public class Issue1796TestCase {
+    @Resource
+    private DruidDataSource dataSource;
+
+    @Test
+    public void test() {
+        assertThat(dataSource.getUrl()).isEqualTo("jdbc:h2:file:./demo-db");
+        assertThat(dataSource.getUsername()).isEqualTo("sa");
+        assertThat(dataSource.getPassword()).isEqualTo("sa");
+        assertThat(dataSource.getDriverClassName()).isEqualTo("org.h2.Driver");
+
+        assertThat(dataSource.getMinEvictableIdleTimeMillis()).isEqualTo(100000);
+        assertThat(dataSource.getMaxEvictableIdleTimeMillis()).isEqualTo(200000);
+    }
+}
