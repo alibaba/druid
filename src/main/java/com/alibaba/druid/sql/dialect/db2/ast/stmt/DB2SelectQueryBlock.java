8f44fb09b48e57b9861352d70e2268c1f28be11a
wenshao <szujobs@hotmail.com>
2017-10-09 20:48:21 +0800

    DB2SelectQueryBlock add toString support. for issue #2034

--- src/main/java/com/alibaba/druid/sql/dialect/db2/ast/stmt/DB2SelectQueryBlock.java
+++ src/main/java/com/alibaba/druid/sql/dialect/db2/ast/stmt/DB2SelectQueryBlock.java
@@ -20,6 +20,7 @@ import com.alibaba.druid.sql.ast.expr.SQLIntegerExpr;
 import com.alibaba.druid.sql.ast.statement.SQLSelectQueryBlock;
 import com.alibaba.druid.sql.dialect.db2.ast.DB2Object;
 import com.alibaba.druid.sql.dialect.db2.visitor.DB2ASTVisitor;
+import com.alibaba.druid.sql.dialect.db2.visitor.DB2OutputVisitor;
 import com.alibaba.druid.sql.visitor.SQLASTVisitor;
 import com.alibaba.druid.util.JdbcConstants;
 
@@ -90,5 +91,10 @@ public class DB2SelectQueryBlock extends SQLSelectQueryBlock implements DB2Objec
         } else {
             throw new UnsupportedOperationException("not support offset");
         }
+    }
+
+    public void output(StringBuffer buf) {
+        DB2OutputVisitor visitor = new DB2OutputVisitor(buf);
+        this.accept(visitor);
     }
 }
