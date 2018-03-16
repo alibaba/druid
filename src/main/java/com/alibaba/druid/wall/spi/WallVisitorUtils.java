fdb1e7061eaf0dfaac38e31fb739db58305f9ab4
黑昼 <kaer.lke@alibaba-inc.com>
2018-03-16 21:34:38 +0800

    fix bug

--- src/main/java/com/alibaba/druid/wall/spi/WallVisitorUtils.java
+++ src/main/java/com/alibaba/druid/wall/spi/WallVisitorUtils.java
@@ -57,7 +57,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
 
 import static com.alibaba.druid.sql.visitor.SQLEvalVisitor.EVAL_VALUE;
 
