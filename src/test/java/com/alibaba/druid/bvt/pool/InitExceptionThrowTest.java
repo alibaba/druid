cece46e9ea309fdd400a6eabc721f9ab364cd00a
wenshao <szujobs@hotmail.com>
2018-08-28 17:32:01 +0800

    add testcase.

--- src/test/java/com/alibaba/druid/bvt/pool/InitExceptionThrowTest.java
+++ src/test/java/com/alibaba/druid/bvt/pool/InitExceptionThrowTest.java
@@ -3,12 +3,13 @@ package com.alibaba.druid.bvt.pool;
 import com.alibaba.druid.mock.MockDriver;
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.pool.DruidPooledConnection;
+import junit.framework.TestCase;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Properties;
 
-public class InitExceptionThrowTest extends TestClone {
+public class InitExceptionThrowTest extends TestCase {
     private DruidDataSource dataSource = new DruidDataSource();
 
     private int connectCount = 0;
