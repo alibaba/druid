76aa751527299c736db1d2bef70ae1b44e80318c
garfield zhang <garfieldv5@gmail.com>
2019-05-30 11:02:30 +0800

    Hive SQL Parser - To use assert statement in unit case

--- src/test/java/com/alibaba/druid/sql/parser/HiveSelectParserTest.java
+++ src/test/java/com/alibaba/druid/sql/parser/HiveSelectParserTest.java
@@ -18,94 +18,75 @@ public class HiveSelectParserTest extends TestCase {
 
 	public void test_limit() throws Exception {
 		String sql = "SELECT col_1, col_2 FROM table_1 LIMIT 5";
+		String expected = "SELECT col_1, col_2\nFROM table_1\nLIMIT 5";
 		List<SQLStatement> stmtList = SQLUtils.parseStatements(sql, JdbcConstants.HIVE);
+		assertEquals(1, stmtList.size());
 		for (SQLStatement stmt : stmtList) {
-			String result = stmt.toString();
-			if (result == null || result.toUpperCase().indexOf("LIMIT") < 0) {
-				throw new Exception("'LIMIT' not found in the result");
-			}
+			assertEquals(expected, stmt.toString());
 		}
 	}
 
 	public void test_sortby() throws Exception {
 		String sql = "SELECT col_1, col_2 FROM table_1 SORT BY col_1 ASC, col_2 DESC";
+		String expected = "SELECT col_1, col_2\nFROM table_1\nSORT BY col_1 ASC, col_2 DESC";
 		List<SQLStatement> stmtList = SQLUtils.parseStatements(sql, JdbcConstants.HIVE);
+		assertEquals(1, stmtList.size());
 		for (SQLStatement stmt : stmtList) {
-			String result = stmt.toString();
-			if (result == null || result.toUpperCase().indexOf("SORT BY") < 0) {
-				throw new Exception("'SORT BY' not found in the result");
-			}
+			assertEquals(expected, stmt.toString());
 		}
 	}
-	
+
 	public void test_distributeby() throws Exception {
 		String sql = "SELECT col_1, col_2 FROM table_1 DISTRIBUTE BY col_1";
+		String expected = "SELECT col_1, col_2\nFROM table_1\nDISTRIBUTE BY col_1";
 		List<SQLStatement> stmtList = SQLUtils.parseStatements(sql, JdbcConstants.HIVE);
+		assertEquals(1, stmtList.size());
 		for (SQLStatement stmt : stmtList) {
-			String result = stmt.toString();
-			if (result == null || result.toUpperCase().indexOf("DISTRIBUTE BY") < 0) {
-				throw new Exception("'DISTRIBUTE BY' not found in the result");
-			}
+			assertEquals(expected, stmt.toString());
 		}
 	}
-	
+
 	public void test_clusterby() throws Exception {
 		String sql = "SELECT col_1, col_2 FROM table_1 CLUSTER BY col_1";
+		String expected = "SELECT col_1, col_2\nFROM table_1\nCLUSTER BY col_1";
 		List<SQLStatement> stmtList = SQLUtils.parseStatements(sql, JdbcConstants.HIVE);
+		assertEquals(1, stmtList.size());
 		for (SQLStatement stmt : stmtList) {
-			String result = stmt.toString();
-			if (result == null || result.toUpperCase().indexOf("CLUSTER BY") < 0) {
-				throw new Exception("'CLUSTER BY' not found in the result");
-			}
+			assertEquals(expected, stmt.toString());
 		}
 	}
-	
+
 	public void test_complex() throws Exception {
 		String sql = "SELECT col_1, col_2 FROM table_1 DISTRIBUTE BY col_1 SORT BY col_1 ASC, col_2 DESC LIMIT 5";
+		String expected = "SELECT col_1, col_2\nFROM table_1\nDISTRIBUTE BY col_1\nSORT BY col_1 ASC, col_2 DESC\nLIMIT 5";
 		List<SQLStatement> stmtList = SQLUtils.parseStatements(sql, JdbcConstants.HIVE);
+		assertEquals(1, stmtList.size());
 		for (SQLStatement stmt : stmtList) {
-			String result = stmt.toString();
-			if (result == null || result.toUpperCase().indexOf("DISTRIBUTE BY") < 0) {
-				throw new Exception("'DISTRIBUTE BY' not found in the result");
-			}
-			if (result == null || result.toUpperCase().indexOf("SORT BY") < 0) {
-				throw new Exception("'SORT BY' not found in the result");
-			}
-			if (result == null || result.toUpperCase().indexOf("LIMIT") < 0) {
-				throw new Exception("'LIMIT' not found in the result");
-			}
+			assertEquals(expected, stmt.toString());
 		}
 	}
 
 	public void test_union_order_limit_1() throws Exception {
-		String sql = "SELECT col_1 col FROM table_1 "
-				+ "union "
-				+ "SELECT col_2 col FROM table_2 "
-				+ "union "
-				+ "SELECT col_3 col FROM table_3 "
-				+ "ORDER BY col ASC LIMIT 5";
+		String sql = "SELECT col_1 col FROM table_1 union SELECT col_2 col FROM table_2 union "
+				+ "SELECT col_3 col FROM table_3 ORDER BY col ASC LIMIT 5";
+		String expected = "SELECT col_1 AS col\nFROM table_1\nUNION\nSELECT col_2 AS col\nFROM table_2\nUNION\n"
+				+ "SELECT col_3 AS col\nFROM table_3\nORDER BY col ASC\nLIMIT 5";
 		List<SQLStatement> stmtList = SQLUtils.parseStatements(sql, JdbcConstants.HIVE);
+		assertEquals(1, stmtList.size());
 		for (SQLStatement stmt : stmtList) {
-			String result = stmt.toString();
-			if (result == null || result.toUpperCase().indexOf("LIMIT") < 0) {
-				throw new Exception("'LIMIT' not found in the result");
-			}
+			assertEquals(expected, stmt.toString());
 		}
 	}
-	
+
 	public void test_union_order_limit_2() throws Exception {
-		String sql = "select tmp.col col from (SELECT col_1 col FROM table_1 ORDER BY col LIMIT 5) tmp "
-				+ "union "
-				+ "SELECT col_2 col FROM table_2 "
-				+ "union "
-				+ "SELECT col_3 col FROM table_3 "
-				+ "ORDER BY col ASC LIMIT 5";
+		String sql = "select tmp.col col from (SELECT col_1 col FROM table_1 ORDER BY col LIMIT 5) tmp union "
+				+ "SELECT col_2 col FROM table_2 union SELECT col_3 col FROM table_3 ORDER BY col ASC LIMIT 5";
+		String expected = "SELECT tmp.col AS col\nFROM (\n\tSELECT col_1 AS col\n\tFROM table_1\n\tORDER BY col\n\tLIMIT 5\n) tmp\n"
+				+ "UNION\nSELECT col_2 AS col\nFROM table_2\nUNION\nSELECT col_3 AS col\nFROM table_3\nORDER BY col ASC\nLIMIT 5";
 		List<SQLStatement> stmtList = SQLUtils.parseStatements(sql, JdbcConstants.HIVE);
+		assertEquals(1, stmtList.size());
 		for (SQLStatement stmt : stmtList) {
-			String result = stmt.toString();
-			if (result == null || result.toUpperCase().indexOf("LIMIT") < 0) {
-				throw new Exception("'LIMIT' not found in the result");
-			}
+			assertEquals(expected, stmt.toString());
 		}
 	}
 }
