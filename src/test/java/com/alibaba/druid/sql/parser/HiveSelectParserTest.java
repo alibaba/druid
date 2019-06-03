0d42d0416b8f4a4a25761090c956d0da8aa0ce27
garfield zhang <garfieldv5@gmail.com>
2019-06-03 15:07:38 +0800

    Formatted SQL in unit test case

--- src/test/java/com/alibaba/druid/sql/parser/HiveSelectParserTest.java
+++ src/test/java/com/alibaba/druid/sql/parser/HiveSelectParserTest.java
@@ -18,7 +18,10 @@ public class HiveSelectParserTest extends TestCase {
 
 	public void test_limit() throws Exception {
 		String sql = "SELECT col_1, col_2 FROM table_1 LIMIT 5";
-		String expected = "SELECT col_1, col_2\nFROM table_1\nLIMIT 5";
+		
+		String expected = "SELECT col_1, col_2\n" //
+				+ "FROM table_1\n" //
+				+ "LIMIT 5"; //
 		List<SQLStatement> stmtList = SQLUtils.parseStatements(sql, JdbcConstants.HIVE);
 		assertEquals(1, stmtList.size());
 		for (SQLStatement stmt : stmtList) {
@@ -28,7 +31,10 @@ public class HiveSelectParserTest extends TestCase {
 
 	public void test_sortby() throws Exception {
 		String sql = "SELECT col_1, col_2 FROM table_1 SORT BY col_1 ASC, col_2 DESC";
-		String expected = "SELECT col_1, col_2\nFROM table_1\nSORT BY col_1 ASC, col_2 DESC";
+		
+		String expected = "SELECT col_1, col_2\n" //
+				+ "FROM table_1\n" //
+				+ "SORT BY col_1 ASC, col_2 DESC"; //
 		List<SQLStatement> stmtList = SQLUtils.parseStatements(sql, JdbcConstants.HIVE);
 		assertEquals(1, stmtList.size());
 		for (SQLStatement stmt : stmtList) {
@@ -38,7 +44,10 @@ public class HiveSelectParserTest extends TestCase {
 
 	public void test_distributeby() throws Exception {
 		String sql = "SELECT col_1, col_2 FROM table_1 DISTRIBUTE BY col_1";
-		String expected = "SELECT col_1, col_2\nFROM table_1\nDISTRIBUTE BY col_1";
+		
+		String expected = "SELECT col_1, col_2\n" //
+				+ "FROM table_1\n" //
+				+ "DISTRIBUTE BY col_1"; //
 		List<SQLStatement> stmtList = SQLUtils.parseStatements(sql, JdbcConstants.HIVE);
 		assertEquals(1, stmtList.size());
 		for (SQLStatement stmt : stmtList) {
@@ -48,7 +57,10 @@ public class HiveSelectParserTest extends TestCase {
 
 	public void test_clusterby() throws Exception {
 		String sql = "SELECT col_1, col_2 FROM table_1 CLUSTER BY col_1";
-		String expected = "SELECT col_1, col_2\nFROM table_1\nCLUSTER BY col_1";
+		
+		String expected = "SELECT col_1, col_2\n" //
+				+ "FROM table_1\n" //
+				+ "CLUSTER BY col_1"; //
 		List<SQLStatement> stmtList = SQLUtils.parseStatements(sql, JdbcConstants.HIVE);
 		assertEquals(1, stmtList.size());
 		for (SQLStatement stmt : stmtList) {
@@ -57,8 +69,16 @@ public class HiveSelectParserTest extends TestCase {
 	}
 
 	public void test_complex() throws Exception {
-		String sql = "SELECT col_1, col_2 FROM table_1 DISTRIBUTE BY col_1 SORT BY col_1 ASC, col_2 DESC LIMIT 5";
-		String expected = "SELECT col_1, col_2\nFROM table_1\nDISTRIBUTE BY col_1\nSORT BY col_1 ASC, col_2 DESC\nLIMIT 5";
+		String sql = "SELECT col_1, col_2 FROM table_1 " //
+				+ "DISTRIBUTE BY col_1 " //
+				+ "SORT BY col_1 ASC, col_2 DESC " //
+				+ "LIMIT 5"; //
+		
+		String expected = "SELECT col_1, col_2\n" //
+				+ "FROM table_1\n" //
+				+ "DISTRIBUTE BY col_1\n" //
+				+ "SORT BY col_1 ASC, col_2 DESC\n" //
+				+ "LIMIT 5"; //
 		List<SQLStatement> stmtList = SQLUtils.parseStatements(sql, JdbcConstants.HIVE);
 		assertEquals(1, stmtList.size());
 		for (SQLStatement stmt : stmtList) {
@@ -67,10 +87,24 @@ public class HiveSelectParserTest extends TestCase {
 	}
 
 	public void test_union_order_limit_1() throws Exception {
-		String sql = "SELECT col_1 col FROM table_1 union SELECT col_2 col FROM table_2 union "
-				+ "SELECT col_3 col FROM table_3 ORDER BY col ASC LIMIT 5";
-		String expected = "SELECT col_1 AS col\nFROM table_1\nUNION\nSELECT col_2 AS col\nFROM table_2\nUNION\n"
-				+ "SELECT col_3 AS col\nFROM table_3\nORDER BY col ASC\nLIMIT 5";
+		String sql = "SELECT col_1 col FROM table_1 " //
+				+ "union " //
+				+ "SELECT col_2 col FROM table_2 " //
+				+ "union " //
+				+ "SELECT col_3 col FROM table_3 " //
+				+ "ORDER BY col ASC " //
+				+ "LIMIT 5"; //
+		
+		String expected = "SELECT col_1 AS col\n" //
+				+ "FROM table_1\n" //
+				+ "UNION\n" //
+				+ "SELECT col_2 AS col\n" //
+				+ "FROM table_2\n" //
+				+ "UNION\n" //
+				+ "SELECT col_3 AS col\n" //
+				+ "FROM table_3\n" //
+				+ "ORDER BY col ASC\n" //
+				+ "LIMIT 5"; //
 		List<SQLStatement> stmtList = SQLUtils.parseStatements(sql, JdbcConstants.HIVE);
 		assertEquals(1, stmtList.size());
 		for (SQLStatement stmt : stmtList) {
@@ -79,10 +113,28 @@ public class HiveSelectParserTest extends TestCase {
 	}
 
 	public void test_union_order_limit_2() throws Exception {
-		String sql = "select tmp.col col from (SELECT col_1 col FROM table_1 ORDER BY col LIMIT 5) tmp union "
-				+ "SELECT col_2 col FROM table_2 union SELECT col_3 col FROM table_3 ORDER BY col ASC LIMIT 5";
-		String expected = "SELECT tmp.col AS col\nFROM (\n\tSELECT col_1 AS col\n\tFROM table_1\n\tORDER BY col\n\tLIMIT 5\n) tmp\n"
-				+ "UNION\nSELECT col_2 AS col\nFROM table_2\nUNION\nSELECT col_3 AS col\nFROM table_3\nORDER BY col ASC\nLIMIT 5";
+		String sql = "select tmp.col col from (SELECT col_1 col FROM table_1 ORDER BY col LIMIT 5) tmp " //
+				+ "union " //
+				+ "SELECT col_2 col FROM table_2 " //
+				+ "union " //
+				+ "SELECT col_3 col FROM table_3 " //
+				+ "ORDER BY col ASC LIMIT 5"; //
+		
+		String expected = "SELECT tmp.col AS col\n" //
+				+ "FROM (\n" //
+				+ "\tSELECT col_1 AS col\n" //
+				+ "\tFROM table_1\n" //
+				+ "\tORDER BY col\n" //
+				+ "\tLIMIT 5\n" //
+				+ ") tmp\n" //
+				+ "UNION\n" //
+				+ "SELECT col_2 AS col\n" //
+				+ "FROM table_2\n" //
+				+ "UNION\n" //
+				+ "SELECT col_3 AS col\n" //
+				+ "FROM table_3\n" //
+				+ "ORDER BY col ASC\n" //
+				+ "LIMIT 5"; //
 		List<SQLStatement> stmtList = SQLUtils.parseStatements(sql, JdbcConstants.HIVE);
 		assertEquals(1, stmtList.size());
 		for (SQLStatement stmt : stmtList) {
