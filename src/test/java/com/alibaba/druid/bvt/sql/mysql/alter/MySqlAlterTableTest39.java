ac064356309625f7d786eb8acdea3d697f10168c
Dagon0577 <dagon0577@gmail.com>
2019-11-07 11:02:10 +0800

    adjust the file location of the test case

---
+++ src/test/java/com/alibaba/druid/bvt/sql/mysql/alter/MySqlAlterTableTest39.java
@@ -0,0 +1,60 @@
+package com.alibaba.druid.bvt.sql.mysql.alter;
+
+import com.alibaba.druid.sql.SQLUtils;
+import com.alibaba.druid.sql.ast.SQLStatement;
+import com.alibaba.druid.util.JdbcConstants;
+import junit.framework.TestCase;
+import org.junit.Assert;
+
+import java.util.List;
+
+/**
+ * @author huangganyan
+ * @date 2019/11/7 10:54
+ */
+public class MySqlAlterTableTest39 extends TestCase {
+    public void test_demo_0() {
+        String sql = "ALTER TABLE `my_dev`.`test_info`\n" + "DROP COLUMN `b`,\n"
+                + "MODIFY COLUMN `a` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '111' AFTER `d`, \n"
+                + "ADD COLUMN `c` varchar(255) NULL COMMENT '333' AFTER `a`";
+
+        List<SQLStatement> stmtList = SQLUtils.parseStatements(sql, JdbcConstants.MYSQL);
+        assertEquals(1, stmtList.size());
+        SQLStatement stmt = stmtList.get(0);
+        String output = SQLUtils.toMySqlString(stmt);
+        assertEquals("ALTER TABLE `my_dev`.`test_info`\n" + "\tDROP COLUMN `b`,\n"
+                + "\tMODIFY COLUMN `a` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '111' AFTER `d`,\n"
+                + "\tADD COLUMN `c` varchar(255) NULL COMMENT '333' AFTER `a`", output);
+
+        sql = "ALTER TABLE `my_dev`.`test_info`\n" + "DROP COLUMN `b`,\n"
+                + "RENAME TO `his_dev`";
+        stmtList = SQLUtils.parseStatements(sql, JdbcConstants.MYSQL);
+        assertEquals(1, stmtList.size());
+        stmt = stmtList.get(0);
+        output = SQLUtils.toMySqlString(stmt);
+        Assert.assertEquals("ALTER TABLE `my_dev`.`test_info`\n" + "\tDROP COLUMN `b`,\n"
+                + "\tRENAME TO `his_dev`",output);
+    }
+
+    public void test_demo_1() {
+        String sql = "ALTER TABLE users ADD grade_id BIGINT DEFAULT NULL, DROP deletedAt, CHANGE username username VARCHAR(50) DEFAULT NULL, CHANGE password user_card_code VARCHAR(255) DEFAULT NULL";
+
+        List<SQLStatement> stmtList = SQLUtils.parseStatements(sql, JdbcConstants.MYSQL);
+        assertEquals(1, stmtList.size());
+        SQLStatement stmt = stmtList.get(0);
+        String output = SQLUtils.toMySqlString(stmt);
+        Assert.assertEquals("ALTER TABLE users\n"
+                + "\tADD COLUMN grade_id BIGINT DEFAULT NULL,\n" + "\tDROP COLUMN deletedAt,\n"
+                + "\tCHANGE COLUMN username username VARCHAR(50) DEFAULT NULL,\n"
+                + "\tCHANGE COLUMN password user_card_code VARCHAR(255) DEFAULT NULL",output);
+
+        sql = "ALTER TABLE promotions_bargain_log CHANGE nickname nickname VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci COMMENT '用户昵称'";
+
+        stmtList = SQLUtils.parseStatements(sql, JdbcConstants.MYSQL);
+        assertEquals(1, stmtList.size());
+        stmt = stmtList.get(0);
+        output = SQLUtils.toMySqlString(stmt);
+        Assert.assertEquals("ALTER TABLE promotions_bargain_log\n"
+                + "\tCHANGE COLUMN nickname nickname VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci COMMENT '用户昵称'",output);
+    }
+}
---
+++ src/test/java/com/alibaba/druid/bvt/sql/mysql/select/MySqlSelectTest_247.java
@@ -0,0 +1,41 @@
+package com.alibaba.druid.bvt.sql.mysql.select;
+
+import com.alibaba.druid.sql.MysqlTest;
+import com.alibaba.druid.sql.SQLUtils;
+import com.alibaba.druid.sql.ast.statement.SQLSelectStatement;
+import com.alibaba.druid.util.JdbcConstants;
+
+/**
+ * @author huangganyan
+ * @date 2019/11/7 10:50
+ */
+public class MySqlSelectTest_247 extends MysqlTest {
+    public void test_0() throws Exception {
+        String  sql = "SELECT DISTINCT k.CONSTRAINT_NAME, k.COLUMN_NAME, k.REFERENCED_TABLE_NAME, "
+                + "k.REFERENCED_COLUMN_NAME /*!50116 , c.update_rule, c.delete_rule */ FROM "
+                + "information_schema.key_column_usage k /*!50116 INNER JOIN "
+                + "information_schema.referential_constraints c ON c.constraint_name = k.constraint_name AND "
+                + "c.table_name = 'activate_log' */ WHERE k.table_name = 'activate_log' AND k.table_schema = "
+                + "'b_yaoking_cn' /*!50116 AND c.constraint_schema = 'b_yaoking_cn' */ AND "
+                + "k.REFERENCED_COLUMN_NAME is not NULL";
+
+
+        SQLSelectStatement stmt = (SQLSelectStatement) SQLUtils
+                .parseSingleStatement(sql, JdbcConstants.MYSQL);
+
+        assertEquals("SELECT DISTINCT k.CONSTRAINT_NAME, k.COLUMN_NAME, k.REFERENCED_TABLE_NAME, k.REFERENCED_COLUMN_NAME /*!50116 , c.update_rule, c.delete_rule */\n"
+                + "FROM information_schema.key_column_usage k /*!50116 INNER JOIN information_schema.referential_constraints c ON c.constraint_name = k.constraint_name AND c.table_name = 'activate_log' */\n"
+                + "WHERE k.table_name = 'activate_log'\n"
+                + "\tAND k.table_schema = 'b_yaoking_cn' /*!50116 AND c.constraint_schema = 'b_yaoking_cn' */\n"
+                + "\tAND k.REFERENCED_COLUMN_NAME IS NOT NULL", stmt.toString());
+
+        sql = "SELECT SysNo,MATCH(Name)AGAINST('张三' WITH QUERY EXPANSION) AS score\n"
+                + "FROM `test_table`\n" + "WHERE MATCH(Name)AGAINST('张三' WITH QUERY EXPANSION)";
+
+        stmt = (SQLSelectStatement) SQLUtils
+                .parseSingleStatement(sql, JdbcConstants.MYSQL);
+
+        assertEquals("SELECT SysNo, MATCH (Name) AGAINST ('张三' WITH QUERY EXPANSION) AS score\n"
+                + "FROM `test_table`\n" + "WHERE MATCH (Name) AGAINST ('张三' WITH QUERY EXPANSION)", stmt.toString());
+    }
+}
