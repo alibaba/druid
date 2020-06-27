6b30a0eda680280997dc1055a26100b202349abf
高铁 <shaojin.wensj@alibaba-inc.com>
2020-06-27 22:58:03 +0800

    update druid version & compile jdk version

--- src/main/java/com/alibaba/druid/VERSION.java
+++ src/main/java/com/alibaba/druid/VERSION.java
@@ -19,7 +19,7 @@ public final class VERSION {
 
     public final static int MajorVersion    = 1;
     public final static int MinorVersion    = 1;
-    public final static int RevisionVersion = 22;
+    public final static int RevisionVersion = 23;
 
     public static String getVersionNumber() {
         return VERSION.MajorVersion + "." + VERSION.MinorVersion + "." + VERSION.RevisionVersion;
