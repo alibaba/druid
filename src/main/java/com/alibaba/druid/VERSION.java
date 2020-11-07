39b58e0474b6b2e4a255ee751674d23b557bc458
高铁 <shaojin.wensj@alibaba-inc.com>
2020-11-07 17:29:45 +0800

    1.2.3 init

--- src/main/java/com/alibaba/druid/VERSION.java
+++ src/main/java/com/alibaba/druid/VERSION.java
@@ -19,7 +19,7 @@ public final class VERSION {
 
     public final static int MajorVersion    = 1;
     public final static int MinorVersion    = 2;
-    public final static int RevisionVersion = 2;
+    public final static int RevisionVersion = 3;
 
     public static String getVersionNumber() {
         return VERSION.MajorVersion + "." + VERSION.MinorVersion + "." + VERSION.RevisionVersion;
