ff9a671fea393feda4cf69ccfba913e6b2eaad9e
高铁 <shaojin.wensj@alibaba-inc.com>
2020-12-26 06:19:07 +0800

    PGDeleteStatement accept support SQLASTVisitor

--- src/main/java/com/alibaba/druid/sql/dialect/postgresql/ast/stmt/PGDeleteStatement.java
+++ src/main/java/com/alibaba/druid/sql/dialect/postgresql/ast/stmt/PGDeleteStatement.java
@@ -48,7 +48,11 @@ public class PGDeleteStatement extends SQLDeleteStatement implements PGSQLStatem
     }
 
     protected void accept0(SQLASTVisitor visitor) {
-        accept0((PGASTVisitor) visitor);
+        if (visitor instanceof PGASTVisitor) {
+            accept0((PGASTVisitor) visitor);
+        } else {
+            super.accept0(visitor);
+        }
     }
 
     @Override
