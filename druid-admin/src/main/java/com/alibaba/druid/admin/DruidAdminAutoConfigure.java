da0fea4bbf1ec105f83a6a174197fa7d6b178adb
yaolinqi <523163881@qq.com>
2021-09-23 11:29:48 +0800

    优化,让使用方选择注册中心,更加灵活

--- druid-admin/src/main/java/com/alibaba/druid/admin/DruidAdminAutoConfigure.java
+++ druid-admin/src/main/java/com/alibaba/druid/admin/DruidAdminAutoConfigure.java
@@ -2,20 +2,14 @@ package com.alibaba.druid.admin;
 
 import com.alibaba.druid.admin.config.MonitorProperties;
 import com.alibaba.druid.admin.servlet.MonitorViewServlet;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
 @EnableConfigurationProperties(MonitorProperties.class)
-public class DruidAdminApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(DruidAdminApplication.class, args);
-    }
-
+public class DruidAdminAutoConfigure {
 
     @Bean
     public ServletRegistrationBean statViewServletRegistrationBean(MonitorProperties properties) {
