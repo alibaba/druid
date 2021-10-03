3f1ef843b369db2a3a913c39fc48d15f628ad822
高铁 <shaojin.wensj@alibaba-inc.com>
2021-10-03 13:11:15 +0800

    refactor testcase

--- src/test/java/com/alibaba/druid/bvt/sql/mysql/issues/Issue4454.java
+++ src/test/java/com/alibaba/druid/bvt/sql/mysql/issues/Issue4454.java
@@ -1,8 +1,13 @@
 package com.alibaba.druid.bvt.sql.mysql.issues;
 
+import com.alibaba.druid.DbType;
+import com.alibaba.druid.sql.SQLUtils;
 import com.alibaba.druid.sql.ast.SQLStatement;
 import com.alibaba.druid.sql.dialect.mysql.parser.MySqlStatementParser;
 import com.alibaba.druid.sql.dialect.mysql.visitor.MySqlSchemaStatVisitor;
+import com.alibaba.druid.sql.parser.SQLParserUtils;
+import com.alibaba.druid.sql.parser.SQLStatementParser;
+import com.alibaba.druid.sql.visitor.SchemaStatVisitor;
 import com.alibaba.druid.stat.TableStat;
 import org.junit.Test;
 
@@ -12,6 +17,8 @@ import java.util.*;
 import static org.junit.Assert.assertFalse;
 
 public class Issue4454 {
+    protected final DbType dbType = DbType.mysql;
+
     @Test
     public void test_idle2() throws Exception {
         String sql = "CREATE TABLE `test_trigger` (\n" +
@@ -19,10 +26,11 @@ public class Issue4454 {
                 "  `test` int(11) signed NOT NULL DEFAULT '0' COMMENT 'trigger test',\n" +
                 "  PRIMARY KEY (`id`)\n" +
                 ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
-        MySqlStatementParser parser = new MySqlStatementParser(sql);
-        SQLStatement statement = parser.parseStatement();
-        MySqlSchemaStatVisitor visitor = new MySqlSchemaStatVisitor();
-        statement.accept(visitor);
+        SQLStatementParser parser = SQLParserUtils.createSQLStatementParser(sql, dbType);
+        SQLStatement stmt = parser.parseStatement();
+        SchemaStatVisitor visitor = SQLUtils.createSchemaStatVisitor(dbType);
+
+        stmt.accept(visitor);
         Map<TableStat.Name, TableStat> tableMap = visitor.getTables();
         assertFalse(tableMap.isEmpty());
     }
