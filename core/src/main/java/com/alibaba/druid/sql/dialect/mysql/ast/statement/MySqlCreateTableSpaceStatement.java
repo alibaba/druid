14a39e7d94c2cfad14da020d9ad61e44c6a4d976
XenoAmess <xenoamess@gmail.com>
2021-10-10 05:25:26 +0800

    several setters in MySqlAlterTablespaceStatement and MySqlCreateTableSpaceStatementmeaningless

--- core/src/main/java/com/alibaba/druid/sql/dialect/mysql/ast/statement/MySqlCreateTableSpaceStatement.java
+++ core/src/main/java/com/alibaba/druid/sql/dialect/mysql/ast/statement/MySqlCreateTableSpaceStatement.java
@@ -66,9 +66,9 @@ public class MySqlCreateTableSpaceStatement extends MySqlStatementImpl implement
         return addDataFile;
     }
 
-    public void setAddDataFile(SQLExpr x) {
-        if (x != null) {
-            x.setParent(this);
+    public void setAddDataFile(SQLExpr addDataFile) {
+        if (addDataFile != null) {
+            addDataFile.setParent(this);
         }
         this.addDataFile = x;
     }
@@ -107,9 +107,9 @@ public class MySqlCreateTableSpaceStatement extends MySqlStatementImpl implement
         return engine;
     }
 
-    public void setEngine(SQLExpr x) {
-        if (x != null) {
-            x.setParent(this);
+    public void setEngine(SQLExpr engine) {
+        if (engine != null) {
+            engine.setParent(this);
         }
         this.engine = engine;
     }
