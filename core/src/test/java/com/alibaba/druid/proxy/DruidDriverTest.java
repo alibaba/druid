86854af01dafc1bd07d892e95258bd5c2c968104
xuwang <flashvip@126.com>
2022-12-07 00:02:27 +0800

    Improved 'jdbc:wrap-jdbc:' url parser function, In previous versions, attribute names must be sequential,such as "jdbc:wrap-jdbc:driver=driver1:filters=filters1:name=name1:jmx=true:jdbc:sqlite:", If not in order, the parsing result will be incorrect.Now,the attribute names can be in any order , such as "jdbc:wrap-jdbc:filters=filters1:jmx=true:name=name1:driver=driver1:jdbc:sqlite:"

---
+++ core/src/test/java/com/alibaba/druid/proxy/DruidDriverTest.java
@@ -0,0 +1,53 @@
+package com.alibaba.druid.proxy;
+
+import com.alibaba.druid.proxy.jdbc.DataSourceProxyConfig;
+import com.alibaba.druid.util.JdbcConstants;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Properties;
+
+public class DruidDriverTest {
+    @Test
+    public void parseConfig() throws Exception {
+       // String url = "jdbc:wrap-jdbc:driver=driver1:filters=filters1:name=name1:jmx=true:jdbc:sqlite:";
+        Properties properties;
+        DataSourceProxyConfig config;
+        String url = "";
+        //have driver property , getRawDriverClassName() = driver1 , getRawUrl()=jdbc:sqlite:
+        url = "jdbc:wrap-jdbc:driver=driver1:filters=com.alibaba.druid.filter.stat.StatFilter,com.alibaba.druid.filter.encoding.EncodingConvertFilter:name=name1:jmx=true:jdbc:sqlite:";
+        properties = new Properties();
+        config = DruidDriver.parseConfig(url,properties);
+        Assert.assertEquals(config.getRawDriverClassName() , "driver1");
+        Assert.assertEquals(config.getName() , "name1");
+        Assert.assertEquals(config.isJmxOption() , true);
+        Assert.assertEquals(config.getRawUrl() , "jdbc:sqlite:");
+
+        //non driver property , getRawDriverClassName() = convert(jdbc:sqlite:) = org.sqlite.JDBC
+        url = "jdbc:wrap-jdbc:name=name2:jmx=true:jdbc:sqlite:";
+        properties = new Properties();
+        config = DruidDriver.parseConfig(url,properties);
+        Assert.assertEquals(config.getName() , "name2");
+        Assert.assertEquals(config.getRawDriverClassName() , JdbcConstants.SQLITE_DRIVER);
+        Assert.assertEquals(config.getRawUrl() , "jdbc:sqlite:");
+
+        //mix properties order, have driver property , getRawDriverClassName() = driver2
+        url = "jdbc:wrap-jdbc:jmx=false:name=name2:filters=com.alibaba.druid.filter.stat.StatFilter:driver=driver2:jdbc:sqlite:";
+        properties = new Properties();
+        config = DruidDriver.parseConfig(url,properties);
+        Assert.assertEquals(config.getRawDriverClassName() , "driver2");
+        Assert.assertEquals(config.getName() , "name2");
+        Assert.assertEquals(config.isJmxOption() , false);
+        Assert.assertEquals(config.getRawUrl() , "jdbc:sqlite:");
+
+        //mix properties order, have driver property , getRawDriverClassName() = driver2
+        url = "jdbc:wrap-jdbc:jmx=true:name=name2:filters=com.alibaba.druid.filter.stat.StatFilter:driver=driver2:jdbc:sqlite:";
+        properties = new Properties();
+        config = DruidDriver.parseConfig(url,properties);
+        Assert.assertEquals(config.getRawDriverClassName() , "driver2");
+        Assert.assertEquals(config.getName() , "name2");
+        Assert.assertEquals(config.isJmxOption() , true);
+        Assert.assertEquals(config.getRawUrl() , "jdbc:sqlite:");
+
+    }
+}
