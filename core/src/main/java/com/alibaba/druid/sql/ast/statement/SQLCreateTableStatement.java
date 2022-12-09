93660779120f3ca9cf3df5d21746edf9067e0a6d
Ekko <1689940684@qq.com>
2022-12-09 19:09:34 +0800

    repository console drop index error

--- core/src/main/java/com/alibaba/druid/sql/ast/statement/SQLCreateTableStatement.java
+++ core/src/main/java/com/alibaba/druid/sql/ast/statement/SQLCreateTableStatement.java
@@ -851,7 +851,7 @@ public class SQLCreateTableStatement extends SQLStatementImpl implements SQLDDLS
             SQLTableElement e = tableElementList.get(i);
             if (e instanceof SQLUniqueConstraint) {
                 SQLUniqueConstraint unique = (SQLUniqueConstraint) e;
-                if (unique.getName().nameHashCode64() == indexNameHashCode64) {
+                if (unique.getName() != null && unique.getName().nameHashCode64() == indexNameHashCode64) {
                     tableElementList.remove(i);
                     return true;
                 }
