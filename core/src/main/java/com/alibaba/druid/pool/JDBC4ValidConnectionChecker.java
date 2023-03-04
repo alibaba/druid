8a983ad06e36b8556a6576f6039411048f81b1b7
kimmking <kimmking@163.com>
2023-03-05 02:57:43 +0800

    add license to JDBC4ValidConnectionChecker

--- core/src/main/java/com/alibaba/druid/pool/JDBC4ValidConnectionChecker.java
+++ core/src/main/java/com/alibaba/druid/pool/JDBC4ValidConnectionChecker.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alibaba.druid.pool;
 
 import java.sql.Connection;
@@ -8,6 +23,7 @@ import java.util.Properties;
  *
  * @Author : kimmking(kimmking@apache.org)
  * @create 2023/3/4 16:48
+ * @Since 1.2.17
  */
 public class JDBC4ValidConnectionChecker implements ValidConnectionChecker {
     @Override
