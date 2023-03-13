2ea7b3af2d160434e310409b3a44310ced28a55c
yanlong <yanlsix@163.com>
2023-03-13 17:52:08 +0800

    presto的create table 语法解析

---
+++ core/src/main/java/com/alibaba/druid/sql/dialect/presto/ast/stmt/PrestoCreateTableStatement.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 1999-2017 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alibaba.druid.sql.dialect.presto.ast.stmt;
+
+import com.alibaba.druid.DbType;
+import com.alibaba.druid.sql.ast.statement.SQLCreateTableStatement;
+import com.alibaba.druid.sql.visitor.SQLASTVisitor;
+
+public class PrestoCreateTableStatement extends SQLCreateTableStatement {
+    public PrestoCreateTableStatement() {
+        this.dbType = DbType.hive;
+    }
+
+    public PrestoCreateTableStatement(DbType dbType) {
+        this.dbType = dbType;
+    }
+
+    protected void accept0(SQLASTVisitor v) {
+        if (v.visit(this)) {
+            acceptChild(v);
+        }
+        v.endVisit(this);
+    }
+
+    protected void acceptChild(SQLASTVisitor v) {
+        super.acceptChild(v);
+    }
+
+    public void cloneTo(PrestoCreateTableStatement x) {
+        super.cloneTo(x);
+    }
+
+    public PrestoCreateTableStatement clone() {
+        PrestoCreateTableStatement x = new PrestoCreateTableStatement();
+        cloneTo(x);
+        return x;
+    }
+}
--- core/src/main/java/com/alibaba/druid/sql/dialect/presto/visitor/PrestoVisitor.java
+++ core/src/main/java/com/alibaba/druid/sql/dialect/presto/visitor/PrestoVisitor.java
@@ -1,6 +1,14 @@
 package com.alibaba.druid.sql.dialect.presto.visitor;
 
+import com.alibaba.druid.sql.dialect.presto.ast.stmt.PrestoCreateTableStatement;
 import com.alibaba.druid.sql.visitor.SQLASTVisitor;
 
 public interface PrestoVisitor extends SQLASTVisitor {
+
+    default boolean visit(PrestoCreateTableStatement x) {
+        return true;
+    }
+
+    default void endVisit(PrestoCreateTableStatement x) {
+    }
 }
