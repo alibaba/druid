284905ebc9a149de1b296dcbb96740948328d3e0
yanlong <yanlsix@163.com>
2023-03-13 23:14:15 +0800

    删除多余换行

--- core/src/test/java/com/alibaba/druid/bvt/sql/presto/PrestoCreateTable_0.java
+++ core/src/test/java/com/alibaba/druid/bvt/sql/presto/PrestoCreateTable_0.java
@@ -15,7 +15,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class PrestoCreateTable_0 {
-
     private final DbType dbType = DbType.presto;
 
     @Test
