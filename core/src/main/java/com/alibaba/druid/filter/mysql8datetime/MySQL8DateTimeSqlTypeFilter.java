c2590f9a0a08ccbfc3c651f272b9748a9e262364
LiZongbo <lizongbo@gmail.com>
2023-05-13 19:09:00 +0800

    修复mvn vlidate 错误 #5274
    修复mvn vlidate 错误 #5274

--- core/src/main/java/com/alibaba/druid/filter/mysql8datetime/MySQL8DateTimeSqlTypeFilter.java
+++ core/src/main/java/com/alibaba/druid/filter/mysql8datetime/MySQL8DateTimeSqlTypeFilter.java
@@ -1,12 +1,12 @@
 package com.alibaba.druid.filter.mysql8datetime;
 
-import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-
 import com.alibaba.druid.filter.FilterAdapter;
 import com.alibaba.druid.filter.FilterChain;
 import com.alibaba.druid.proxy.jdbc.ResultSetProxy;
+
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 /**
  * 针对mysql jdbc 8.0.23及以上版本，如果调用方没有使用orm框架，而是直接调用ResultSet的getObject方法，则针对DATETIME类型的字段，得到的对象从TimeStamp类型变成了LocalDateTime类型，导致调用方出现类型转换异常
@@ -16,15 +16,15 @@ import com.alibaba.druid.proxy.jdbc.ResultSetProxy;
  * @see <a href="https://dev.mysql.com/doc/relnotes/connector-j/8.0/en/news-8-0-23.html">MySQL 8.0.23 更新说明</a>
  */
 public class MySQL8DateTimeSqlTypeFilter extends FilterAdapter {
-
     /**
      * 针对mysql jdbc 8.0.23及以上版本，通过该方法控制将对象类型转换成原来的类型
+     *
      * @param chain
      * @param result
      * @param columnIndex
      * @return
-     * @see java.sql.ResultSet#getObject(int)
      * @throws SQLException
+     * @see java.sql.ResultSet#getObject(int)
      */
     @Override
     public Object resultSet_getObject(FilterChain chain, ResultSetProxy result, int columnIndex) throws SQLException {
@@ -33,12 +33,13 @@ public class MySQL8DateTimeSqlTypeFilter extends FilterAdapter {
 
     /**
      * 针对mysql jdbc 8.0.23及以上版本，通过该方法控制将对象类型转换成原来的类型
+     *
      * @param chain
      * @param result
      * @param columnLabel
      * @return
-     * @see java.sql.ResultSet#getObject(String)
      * @throws SQLException
+     * @see java.sql.ResultSet#getObject(String)
      */
     @Override
     public Object resultSet_getObject(FilterChain chain, ResultSetProxy result, String columnLabel) throws SQLException {
