350a5fc7c0b84216bdfe1c16ddfe8260e9c3c952
zhenggli <zguan.li2@gmail.com>
2024-01-30 14:43:09 -0600

    Update test to use string concatenation with +

--- core/src/test/java/com/alibaba/druid/sql/dialect/oracle/parser/OracleParameterParserTest.java
+++ core/src/test/java/com/alibaba/druid/sql/dialect/oracle/parser/OracleParameterParserTest.java
@@ -11,32 +11,29 @@ import java.util.List;
 public class OracleParameterParserTest {
 	@Test
 	public void testTableOfParameter() {
-		String sql = """
-					DECLARE
-						TYPE Foursome IS TABLE OF VARCHAR2(15);
-						team Foursome := Foursome('John', 'Mary', 'Alberto', 'Juanita');
-					BEGIN
-						DBMS_OUTPUT.PUT_LINE('2001 Team:');
-						FOR i IN 1..4
-						LOOP
-							DBMS_OUTPUT.PUT_LINE(i || '.' || team(i));
-					  	END LOOP;
-					END;
-				""";
-		String expectedSql = """
-					[DECLARE
-						TYPE Foursome IS TABLE OF VARCHAR2(15);
-						team Foursome := Foursome('John', 'Mary', 'Alberto', 'Juanita');
-					BEGIN
-						DBMS_OUTPUT.PUT_LINE('2001 Team:');
-						FOR i IN 1..4
-						LOOP
-							DBMS_OUTPUT.PUT_LINE(i || '.' || team(i));
-						END LOOP;
-					END;]\
-					""";
+		String sql = "DECLARE\n" +
+			"	TYPE Foursome IS TABLE OF VARCHAR2(15);\n" +
+			"	team Foursome := Foursome('John', 'Mary', 'Alberto', 'Juanita');\n" +
+			"BEGIN\n" +
+			"	DBMS_OUTPUT.PUT_LINE('2001 Team:');\n" +
+			"	FOR i IN 1..4\n" +
+			"	LOOP\n" +
+			"		DBMS_OUTPUT.PUT_LINE(i || '.' || team(i));\n" +
+			"	END LOOP;\n" +
+			"END;\n";
+
+		String expectedSql = "[DECLARE\n" +
+			"	TYPE Foursome IS TABLE OF VARCHAR2(15);\n" +
+			"	team Foursome := Foursome('John', 'Mary', 'Alberto', 'Juanita');\n" +
+			"BEGIN\n" +
+			"	DBMS_OUTPUT.PUT_LINE('2001 Team:');\n" +
+			"	FOR i IN 1..4\n" +
+			"	LOOP\n" +
+			"		DBMS_OUTPUT.PUT_LINE(i || '.' || team(i));\n" +
+			"	END LOOP;\n" +
+			"END;]";
 		List<SQLStatement> stat = SQLUtils.parseStatements(sql, DbType.oracle);
-		System.out.println(stat.toString());
+		System.out.println(stat);
 		Assert.assertEquals(expectedSql, stat.toString());
 		System.out.println("=============");
 	}
