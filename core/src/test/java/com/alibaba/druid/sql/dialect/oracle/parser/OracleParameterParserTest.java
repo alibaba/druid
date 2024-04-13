c724605c5a069db603b6e48df78ca2d364a2b014
zhenggli <zguan.li2@gmail.com>
2024-04-12 19:38:50 -0500

    Improve INDEX BY table parsing

--- core/src/test/java/com/alibaba/druid/sql/dialect/oracle/parser/OracleParameterParserTest.java
+++ core/src/test/java/com/alibaba/druid/sql/dialect/oracle/parser/OracleParameterParserTest.java
@@ -24,4 +24,25 @@ public class OracleParameterParserTest {
 		Assert.assertEquals(sql, stat.toString());
 		System.out.println("=============");
 	}
+
+	@Test
+	public void testNestedParameter_NumberIndexBy() {
+		String sql = "DECLARE\n" +
+					 "\tTYPE NUMBER_ARRAY_TYPE IS TABLE OF NUMBER INDEX BY BINARY_INTEGER;\n" +
+					 "\tmyArray NUMBER_ARRAY_TYPE;\n" +
+					 "BEGIN\n" +
+					 "\tFOR i IN 1..10\n" +
+					 "\tLOOP\n" +
+					 "\t\tmyArray(i) := i * 10;\n" +
+					 "\tEND LOOP;\n" +
+					 "\tFOR i IN 1..10\n" +
+					 "\tLOOP\n" +
+					 "\t\tDBMS_OUTPUT.PUT_LINE('Index: ' || i || ', Value: ' || myArray(i));\n" +
+					 "\tEND LOOP;\n" +
+					 "END;";
+		SQLStatement stat = SQLUtils.parseSingleStatement(sql, DbType.oracle, false);
+		System.out.println(stat);
+		Assert.assertEquals(sql, stat.toString());
+		System.out.println("=============");
+	}
 }
