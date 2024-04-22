c0d96da3f21363e84667087402ee659c39d50f3b
zhenggli <zguan.li2@gmail.com>
2024-04-22 16:35:23 -0500

    Support NAME keyword in Oracle XMLELEMENT function

---
+++ core/src/test/java/com/alibaba/druid/bvt/sql/oracle/OracleXmlelementTest.java
@@ -0,0 +1,45 @@
+package com.alibaba.druid.bvt.sql.oracle;
+
+import com.alibaba.druid.sql.OracleTest;
+import com.alibaba.druid.sql.ast.SQLStatement;
+import com.alibaba.druid.sql.dialect.oracle.parser.OracleStatementParser;
+import com.alibaba.druid.sql.dialect.oracle.visitor.OracleSchemaStatVisitor;
+import org.junit.Assert;
+
+import java.util.List;
+
+public class OracleXmlelementTest extends OracleTest {
+    public void testOracleXmlelement() {
+        String sql = "SELECT /* NUSQL.TEST */ XMLELEMENT(NAME foo).getstringval() from dual";
+
+        OracleStatementParser parser = new OracleStatementParser(sql);
+        List<SQLStatement> statementList = parser.parseStatementList();
+        SQLStatement statement = statementList.get(0);
+        print(statementList);
+
+        Assert.assertEquals(1, statementList.size());
+
+        OracleSchemaStatVisitor visitor = new OracleSchemaStatVisitor();
+        statement.accept(visitor);
+
+        Assert.assertEquals(0, visitor.getTables().size());
+        Assert.assertEquals(0, visitor.getColumns().size());
+    }
+
+    public void testOracleXmlelement_WithoutNameKeyword() {
+        String sql = "SELECT /* NUSQL.TEST */ XMLELEMENT(foo).getstringval() from dual";
+
+        OracleStatementParser parser = new OracleStatementParser(sql);
+        List<SQLStatement> statementList = parser.parseStatementList();
+        SQLStatement statement = statementList.get(0);
+        print(statementList);
+
+        Assert.assertEquals(1, statementList.size());
+
+        OracleSchemaStatVisitor visitor = new OracleSchemaStatVisitor();
+        statement.accept(visitor);
+
+        Assert.assertEquals(0, visitor.getTables().size());
+        Assert.assertEquals(0, visitor.getColumns().size());
+    }
+}
