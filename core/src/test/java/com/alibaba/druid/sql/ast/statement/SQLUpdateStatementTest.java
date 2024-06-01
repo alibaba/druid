a2a74eaf94d64edc2343ea72d991c0786a838e0d
ly1546644813 <50403457+ly1546644813@users.noreply.github.com>
2024-06-01 23:35:47 +0800

    Update SQLUpdateStatementTest.java
    update语句多个join表明获取异常测试用例

--- core/src/test/java/com/alibaba/druid/sql/ast/statement/SQLUpdateStatementTest.java
+++ core/src/test/java/com/alibaba/druid/sql/ast/statement/SQLUpdateStatementTest.java
@@ -1,23 +1,28 @@
 package com.alibaba.druid.sql.ast.statement;
 
 import com.alibaba.druid.DbType;
+import com.alibaba.druid.sql.ast.SQLName;
+import com.alibaba.druid.sql.builder.SQLBuilderFactory;
+import com.alibaba.druid.sql.builder.SQLUpdateBuilder;
+import com.alibaba.druid.sql.builder.impl.SQLUpdateBuilderImpl;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class SQLUpdateStatementTest {
     @Test
     public void testGetTableName() {
-        SQLUpdateStatement sqlUpdateStatement = new SQLUpdateStatement();
-        SQLJoinTableSource tableSource1 = new SQLJoinTableSource();
-        SQLJoinTableSource tableSource2 = new SQLJoinTableSource();
-        SQLExprTableSource tableSource3 = new SQLExprTableSource();
-        tableSource3.setExpr("xxx");
-        tableSource2.setLeft(tableSource3);
-        tableSource1.setLeft(tableSource2);
-        sqlUpdateStatement.setDbType(DbType.mysql);
-        sqlUpdateStatement.setTableSource(tableSource1);
-
-        assertEquals(sqlUpdateStatement.getTableName(true), tableSource3.getName());
+        SQLUpdateBuilderImpl updateBuilder = (SQLUpdateBuilderImpl)SQLBuilderFactory.createUpdateBuilder("UPDATE a\n" +
+                "LEFT JOIN b ON b.id = a.id\n" +
+                "LEFT JOIN c ON c.id = a.id\n" +
+                "set a.id=11,b.id=11,c.id=11", DbType.mysql);
+        SQLUpdateStatement sqlUpdateStatement = updateBuilder.getSQLUpdateStatement();
+        SQLName tableName = sqlUpdateStatement.getTableName();
+        String simpleName = sqlUpdateStatement.getTableName(true).getSimpleName();
+        // 原有逻辑返回null
+        assertTrue(tableName == null);
+        // 新增逻辑 返回表名a
+        assertTrue(simpleName.equals("a"));
     }
 }
