18ef68123b895855c9b92cf09c52d4e086e3e534
Ekko <48896971+guangchaozhou@users.noreply.github.com>
2024-07-10 15:55:20 +0800

    change utf8 类型

--- core/src/main/java/com/alibaba/druid/sql/SQLUtils.java
+++ core/src/main/java/com/alibaba/druid/sql/SQLUtils.java
@@ -70,6 +70,7 @@ import com.alibaba.druid.support.logging.LogFactory;
 import com.alibaba.druid.util.*;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
@@ -79,7 +80,7 @@ import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class SQLUtils {
-    public static final Charset UTF8 = Charset.forName("UTF-8");
+    public static final Charset UTF8 = StandardCharsets.UTF_8;
 
     private static final SQLParserFeature[] FORMAT_DEFAULT_FEATURES = {
             SQLParserFeature.KeepComments,
