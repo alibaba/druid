bbf5f8346ee6252172d2d4d05deaddfbd3bf80a7
林枸 <lingou.xp@alibaba-inc.com>
2024-08-09 18:44:46 +0800

    Fix.

--- core/src/main/java/com/alibaba/druid/sql/dialect/impala/visitor/ImpalaASTVisitor.java
+++ core/src/main/java/com/alibaba/druid/sql/dialect/impala/visitor/ImpalaASTVisitor.java
@@ -2,11 +2,6 @@ package com.alibaba.druid.sql.dialect.impala.visitor;
 
 import com.alibaba.druid.sql.dialect.impala.stmt.ImpalaCreateTableStatement;
 
-/**
- * @author lingo
- * @date 2024/8/9 17:46
- * @description
- */
 public interface ImpalaASTVisitor {
     default boolean visit(ImpalaCreateTableStatement x) {
         return true;
