1b76d73610be1d122fc9440b38d3503a19a043d6
啊西 <jinrongchuan.jrc@alibaba-inc.com>
2024-09-09 17:41:26 +0800

    impala parser support upsert followed by with clause

--- core/src/main/java/com/alibaba/druid/sql/dialect/impala/parser/ImpalaLexer.java
+++ core/src/main/java/com/alibaba/druid/sql/dialect/impala/parser/ImpalaLexer.java
@@ -4,11 +4,17 @@ import com.alibaba.druid.DbType;
 import com.alibaba.druid.sql.dialect.hive.parser.HiveLexer;
 import com.alibaba.druid.sql.parser.Keywords;
 import com.alibaba.druid.sql.parser.SQLParserFeature;
+import com.alibaba.druid.sql.parser.Token;
+
+import java.util.Map;
 
 public class ImpalaLexer extends HiveLexer {
     @Override
     protected Keywords loadKeywords() {
-        return super.loadKeywords();
+        Keywords keywords = super.loadKeywords();
+        Map<String, Token> map = keywords.getKeywords();
+        map.put("UPSERT", Token.UPSERT);
+        return new Keywords(map);
     }
 
     public ImpalaLexer(String input) {
