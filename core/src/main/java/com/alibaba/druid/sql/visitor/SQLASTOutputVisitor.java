d8c298b55ade6e46606dba9930a224263c86e803
zhenggli <zguan.li2@gmail.com>
2024-09-25 21:34:26 -0500

    Add Parentheses only if they are present in the SQLListExpr

--- core/src/main/java/com/alibaba/druid/sql/visitor/SQLASTOutputVisitor.java
+++ core/src/main/java/com/alibaba/druid/sql/visitor/SQLASTOutputVisitor.java
@@ -4709,9 +4709,13 @@ public class SQLASTOutputVisitor extends SQLASTVisitorAdapter implements Paramet
 
     @Override
     public boolean visit(SQLListExpr x) {
-        print('(');
+        if (x.isParenthesized()) {
+            print('(');
+        }
         printAndAccept(x.getItems(), ", ");
-        print(')');
+        if (x.isParenthesized()) {
+            print(')');
+        }
 
         return false;
     }
