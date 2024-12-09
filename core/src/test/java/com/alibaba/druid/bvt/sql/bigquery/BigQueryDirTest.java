abfcd406c052d028874acd2d085005cf3bfe8935
林枸 <lingou.xp@alibaba-inc.com>
2024-12-09 15:30:07 +0800

    Fix parse array with data type.

--- core/src/test/java/com/alibaba/druid/bvt/sql/bigquery/BigQueryDirTest.java
+++ core/src/test/java/com/alibaba/druid/bvt/sql/bigquery/BigQueryDirTest.java
@@ -4,6 +4,7 @@ import com.alibaba.druid.DbType;
 import com.alibaba.druid.bvt.sql.SQLResourceTest;
 import com.alibaba.druid.sql.SQLUtils;
 import org.apache.commons.io.FileUtils;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
@@ -20,25 +21,38 @@ public class BigQueryDirTest extends SQLResourceTest {
         return new File(path);
     }
 
+    @Ignore
     @Test
     public void dirTest() throws Exception {
-//        File dir = new File("/Users/wenshao/Downloads/goto_1894_sql");
-//        File dir = new File("/Users/wenshao/Downloads/BigQuery");
-//        File[] files = dir.listFiles();
-//        Arrays.sort(files, Comparator.comparing(e -> e.getName().toLowerCase()));
-//        for (File file : files) {
-//            if (file.getName().equals(".DS_Store")) {
-//                continue;
-//            }
-//
-//            System.out.println(file.getAbsolutePath());
-//            String sql = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
-//            try {
-//                SQLUtils.parseStatements(sql, DbType.bigquery);
-//                file.delete();
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }
+    //        File dir = new File("/Users/wenshao/Downloads/goto_1894_sql");
+        File dir = new File("/Users/lingo/workspace/alibaba/druid/goto/all_etl_jobs/");
+        File[] files = dir.listFiles();
+        Arrays.sort(files, Comparator.comparing(e -> e.getName().toLowerCase()));
+        long total = files.length;
+        long success = 0L;
+        for (File file : files) {
+            if (file.getName().equals(".DS_Store")) {
+                continue;
+            }
+
+            
+            String sql = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+            try {
+                SQLUtils.parseStatements(sql, DbType.bigquery);
+                success += 1;
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println(file.getAbsolutePath());
+            }
+        }
+        System.out.println("success: " + success + "/" + total);
+    }
+
+    @Ignore
+    @Test
+    public void fileTest() throws Exception {
+        File file = new File("/Users/lingo/workspace/alibaba/druid/goto/all_etl_jobs/gojek-mart-kafka-gofood_challenges_campaign.sql");
+        String sql = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+        SQLUtils.parseStatements(sql, DbType.bigquery);
     }
 }
