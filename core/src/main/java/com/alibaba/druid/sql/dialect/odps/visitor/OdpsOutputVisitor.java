09e8c832c5f3a73b61f4e81a016e7064b61a6b62
林枸 <lingou.xp@alibaba-inc.com>
2024-12-12 17:38:50 +0800

    Modify odps struct output.

--- core/src/main/java/com/alibaba/druid/sql/dialect/odps/visitor/OdpsOutputVisitor.java
+++ core/src/main/java/com/alibaba/druid/sql/dialect/odps/visitor/OdpsOutputVisitor.java
@@ -1051,33 +1051,6 @@ public class OdpsOutputVisitor extends HiveOutputVisitor implements OdpsASTVisit
         return false;
     }
 
-    public boolean visit(SQLStructExpr x) {
-        List<SQLAliasedExpr> items = x.getItems();
-        int aliasCount = 0;
-        for (int i = 0, size = items.size(); i < size; ++i) {
-            SQLAliasedExpr item = items.get(i);
-            if (item.getAlias() != null) {
-                aliasCount++;
-            }
-        }
-        if (aliasCount != items.size()) {
-            return super.visit(x);
-        }
-
-        print0(ucase ? "NAMED_STRUCT(" : "named_struct(");
-        for (int i = 0, size = items.size(); i < size; ++i) {
-            if (i != 0) {
-                print0(", ");
-            }
-            SQLAliasedExpr item = items.get(i);
-            visit(new SQLIdentifierExpr(item.getAlias()));
-            print0(", ");
-            item.getExpr().accept(this);
-        }
-        print(')');
-        return false;
-    }
-
     @Override
     public boolean visit(SQLCurrentTimeExpr x) {
         final SQLCurrentTimeExpr.Type type = x.getType();
