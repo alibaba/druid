a6a483ee3e4da7df70ab9ca6e33c6eda6a10e76c
zyc <zyc@hasor.net>
2025-01-17 17:56:47 +0800

    1.support doris AUTO PARTITION BY RANGE.

--- core/src/main/java/com/alibaba/druid/sql/dialect/doris/parser/DorisCreateTableParser.java
+++ core/src/main/java/com/alibaba/druid/sql/dialect/doris/parser/DorisCreateTableParser.java
@@ -1,6 +1,7 @@
 package com.alibaba.druid.sql.dialect.doris.parser;
 
 import com.alibaba.druid.DbType;
+import com.alibaba.druid.sql.ast.SQLPartitionBy;
 import com.alibaba.druid.sql.dialect.starrocks.parser.StarRocksCreateTableParser;
 import com.alibaba.druid.sql.parser.SQLExprParser;
 
@@ -9,5 +10,17 @@ public class DorisCreateTableParser
     public DorisCreateTableParser(SQLExprParser exprParser) {
         super(exprParser);
         dbType = DbType.doris;
+    }
+
+    public SQLPartitionBy parsePartitionBy() {
+        if (lexer.nextIfIdentifier("AUTO")) {
+            SQLPartitionBy partitionBy = super.parsePartitionBy();
+            if (partitionBy != null) {
+                partitionBy.setAuto(true);
+            }
+            return partitionBy;
+        } else {
+            return super.parsePartitionBy();
+        }
     }
 }
