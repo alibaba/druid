78dca6326b6ae0b07eed12b7ac779590c44bfe62
林枸 <lingou.xp@alibaba-inc.com>
2025-02-03 18:31:52 +0800

    Implement databricks create or replace temp table.

--- core/src/main/java/com/alibaba/druid/sql/dialect/databricks/parser/DatabricksCreateTableParser.java
+++ core/src/main/java/com/alibaba/druid/sql/dialect/databricks/parser/DatabricksCreateTableParser.java
@@ -5,6 +5,7 @@ import com.alibaba.druid.sql.dialect.spark.parser.SparkCreateTableParser;
 import com.alibaba.druid.sql.parser.SQLExprParser;
 import com.alibaba.druid.sql.parser.SQLSelectParser;
 import com.alibaba.druid.sql.parser.Token;
+import com.alibaba.druid.util.FnvHash;
 
 public class DatabricksCreateTableParser extends SparkCreateTableParser {
     public DatabricksCreateTableParser(SQLExprParser exprParser) {
@@ -18,6 +19,11 @@ public class DatabricksCreateTableParser extends SparkCreateTableParser {
         if (lexer.nextIf(Token.OR)) {
             accept(Token.REPLACE);
             createTable.config(SQLCreateTableStatement.Feature.OrReplace);
+        }
+
+        if (lexer.identifierEquals(FnvHash.Constants.TEMPORARY) || lexer.identifierEquals("temp")) {
+            lexer.nextToken();
+            createTable.setTemporary(true);
         }
     }
 }
