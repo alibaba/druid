568a1a6ec87b5411daf861cdbca8e6b68d5f997e
Muhong_Yang <53040786+cheetahburg@users.noreply.github.com>
2025-04-01 20:40:20 +0800

    add keyword config for spark/hive (#6390)
    * add dbtype adb_mysql
    
    * Presto/Trino parser fix
    
    * add CTE visit for insertNode
    
    * add keyword config for spark/hive

---
+++ core/src/test/java/com/alibaba/druid/bvt/sql/hive/HiveKeywordsTest.java
@@ -0,0 +1,28 @@
+package com.alibaba.druid.bvt.sql.hive;
+
+import com.alibaba.druid.DbType;
+import com.alibaba.druid.sql.SQLUtils;
+import com.alibaba.druid.sql.ast.SQLStatement;
+import com.alibaba.druid.sql.parser.SQLParserFeature;
+import com.alibaba.druid.sql.parser.SQLParserUtils;
+import com.alibaba.druid.sql.parser.SQLStatementParser;
+import com.alibaba.druid.sql.parser.Token;
+import com.alibaba.druid.sql.visitor.VisitorFeature;
+import junit.framework.TestCase;
+import org.junit.Assert;
+
+public class HiveKeywordsTest extends TestCase {
+
+    public void test_keywords() {
+        DbType dbType = DbType.hive;
+        String sql = "select 1 as TIMESTAMPLOCALTZ";
+        SQLStatementParser parser = SQLParserUtils.createSQLStatementParser(sql,
+                dbType,
+                SQLParserFeature.IgnoreNameQuotes);
+        SQLStatement stmt = parser.parseStatement();
+        assertEquals(Token.EOF, parser.getLexer().token());
+        String result = SQLUtils.toSQLString(stmt, dbType, null, VisitorFeature.OutputNameQuote).trim();
+        String expectedSql = "SELECT 1 AS `TIMESTAMPLOCALTZ`";
+        Assert.assertEquals(expectedSql, result);
+    }
+}
