e069fb86a570d8cd4b375d42b681f400044b99fa
林枸 <lingou.xp@alibaba-inc.com>
2025-10-21 13:45:21 +0800

    Rename.

--- core/src/test/java/com/alibaba/druid/bvt/sql/lexer/LexerSavePointResetTest.java
+++ core/src/test/java/com/alibaba/druid/bvt/sql/lexer/LexerSavePointResetTest.java
@@ -11,7 +11,7 @@ import org.apache.commons.lang3.tuple.Pair;
 import java.util.List;
 import java.util.Map;
 
-public class SavePointResetTest extends TestCase {
+public class LexerSavePointResetTest extends TestCase {
     /**
      * Test reset with SavePoint - verify tokens are properly cleaned up after reset
      */
