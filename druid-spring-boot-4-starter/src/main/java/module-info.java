b94e843e823e349e6a948cb7bdbe1fe10c7c3b18
小墨 <16949192+coder-xiaomo@users.noreply.github.com>
2025-11-21 08:54:04 +0800

    Add spring boot 4.x support (#6536)

---
+++ druid-spring-boot-4-starter/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+open module druid.spring.boot4.starter {
+    exports com.alibaba.druid.spring.boot4.autoconfigure;
+    exports com.alibaba.druid.spring.boot4.autoconfigure.stat;
+    exports com.alibaba.druid.spring.boot4.autoconfigure.properties;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires spring.core;
+    requires spring.aop;
+    requires spring.beans;
+    requires druid;
+    requires org.slf4j;
+    requires java.sql;
+    requires java.naming;
+    requires java.management;
+    requires spring.boot.jdbc;
+}
